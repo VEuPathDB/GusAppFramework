4f0c53de84fec7660012740bddd4b3a41f8bf6e2
David Barkan <dbarkan@_.com>
2003-04-10 14:29:07 +0000

    changed RemoteJDBCServer to read values in from properties file
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/GUS/trunk@1238 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- ObjRelJ/src/java/org/gusdb/objrelj/JDBCDatabaseConnection.java
+++ ObjRelJ/src/java/org/gusdb/objrelj/JDBCDatabaseConnection.java
@@ -165,12 +165,9 @@ public class JDBCDatabaseConnection implements DatabaseConnectionI {
 	    ResultSet res = stmt.executeQuery(sql);
 
 	    while (res.next()) {
-		System.err.println ("DTB: processing next " + tname);
 		++numReturned;
 		obj = GUSRow.createObject(owner, tname);
-		System.err.println ("DTB: setting atts for " + tname);
 		obj.setAttributesFromResultSet(res, specialCases);
-		System.err.println ("DTB: atts set for " + tname);
 
 	    }
 	    res.close();
@@ -179,21 +176,12 @@ public class JDBCDatabaseConnection implements DatabaseConnectionI {
 	    System.err.println(e.getMessage());
 	    e.printStackTrace(); 
 	}
-	System.err.println ("DTB: JDBCDatabaseConnection: finished retrieving " + tname);
 	if (numReturned != 1) {
 	    throw new GUSObjectNotUniqueException("Found " + numReturned + " rows in " + owner + "." + 
 						  tname + " with id=" + pk);
 	}
-	else{
-	    System.err.println ("DTB: Found " + numReturned + " rows in " + owner + "." + 
-				tname + " with id=" + pk);
-	}
-	if (obj == null){ System.err.println("DTB: Object is still null");}
-	else {
-	    System.err.println("returning object that is not null of type " + tname);
-	}
+	
 	return obj;
-       
     }
 
     public Vector retrieveObjectsFromQuery(String owner, String tname, String query)
