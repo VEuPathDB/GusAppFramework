5869afb6572da7c5824dfb656f527c260982bb01
David Barkan <dbarkan@_.com>
2003-06-11 00:58:42 +0000

    updated to add more preprocessing functionality
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/GUS/trunk@1398 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- GOPredict/plugin/perl/FixDeletedProteins.pm
+++ GOPredict/plugin/perl/FixDeletedProteins.pm
@@ -2,6 +2,9 @@ package GUS::GOPredict::Plugin::FixDeletedProteins;
 @ISA = qw( GUS::PluginMgr::Plugin);
 
 
+use GUS::Model::DoTS::GOAssociation;
+use GUS::Model::DoTS::GOAssociationInstance;
+
 use lib "$ENV{GUS_HOME}/lib/perl";
 
 use strict 'vars';
@@ -22,10 +25,18 @@ sub new{
     my $easycsp =
 	[
 	 
-#	 {o=> 'flat_file',
-#	  h=> 'read data from this flat_file.  If blank, read data from all .ontology files in filepath',
-#          t=> 'string',
-#      }
+	 {o=> 'recover_deleted',
+	  h=> 'used to recover deleted associations between proteins and GO Terms',
+          t=> 'boolean',
+      }
+	 {o=> 'deprecate_automated_instances',
+	  h=> 'used to deprecate all instances that are not manually reviewed',
+          t=> 'boolean',
+      }
+	 {o=> 'deprecate_old_instances',
+	  h=> 'used to deprecate instances older than the current go version',
+          t=> 'boolean',
+      }
 
 	 ];
     
@@ -45,18 +56,41 @@ sub isReadOnly {0};
 
 sub run {
     my ($self) = @_;
-
-    my $proteinGoMap = $self->_getProteinGoMap();
-    my $gusGoMap = $self->_makeGoMap();
-    $self->_findMissingProteins($proteinGoMap, $gusGoMap);
+    
+    my $msg;
+    if ($self->getCla->{recover_deleted}){
+	
+	my $gusGoMap = $self->_makeGoMap();
+	my $proteinGoMap = $self->_getProteinGoMap();
+   
+	$msg = $self->_findMissingProteins($proteinGoMap, $gusGoMap);
+	return $msg;
+    }
+    else if ($self->getCla->{deprecate_automated_instances}){
+	
+	$self->_setAllInstancesToPrimary();
+	my $msg = $self->_deprecateAutomatedInstances();
+	return $msg;
+    }
+    
+    else if ($self->getCla->{deprecate_old_instances}){
+	
+	my $msg = $self->_deprecateOldInstances();
+	return $msg;
+    }
+    else {
+	$self->userError("please select a valid mode");
+    }
 }
 
 sub _findMissingProteins{
     
     my ($self, $proteinGoMap, $gusGoMap) = @_;
+
+    my $changeCount = 0;
     
     my $queryHandle = $self->getQueryHandle();
-    my $sql = "select ga.row_id, ga.go_term_id 
+    my $sql = "select distinct ga.row_id, ga.go_term_id 
                from sres.goterm gt, dots.goAssociation ga, 
                sres.externaldatabaserelease r,
                dots.translatedAAsequence ts, dots.rnaInstance rs, dots.NaFeature f,
@@ -64,7 +98,7 @@ sub _findMissingProteins{
                where ts.aa_sequence_id = ga.row_id
                and ga.table_id = 337
                and gt.go_term_id = ga.go_term_id
-               and r.external_database_release_id = $goExtDb
+               and gt.external_database_release_id = $goExtDb
                and ts.aa_sequence_id = tf.aa_sequence_id
                and tf.na_feature_id = f.na_feature_id
                and f.na_feature_id = rs.na_feature_id
@@ -72,36 +106,29 @@ sub _findMissingProteins{
                and p.rna_id = rna.rna_id
                and p.protein_id in ?";
 
-    my $sth = $queryHandle->prepare();
+    my $sth = $queryHandle->prepare($sql);
+    
+    my $proteinChangeCount;
+
     foreach my $proteinId (keys %$proteinGoMap){
 	$sth->execute($proteinId);
-
+	
 	while (my ($taasId, $goTermId) = $sth->fetchrow_array()){
 	    
 	    if (!$proteinGoMap->{$proteinId}->{$goTermId}){
+		$proteinChangeCount->{$proteinId} = 1;
+		$changeCount++;
+		    
 		$self->_makeGoAssociation($proteinId, $goTermId, $gusGoMap->{$goTermId});
 	    }
 	}
     }
-}
 
-sub _makeGoMap{
+    my $proteinsChanged = scalar keys %$proteinChangeCount;
 
-    my ($self) = @_;
+    my $msg = "Made $changeCount new assocations and instances with is_not set to 1 for $proteinsChanged Proteins";
 
-    my $queryHandle = $self->getQueryHandle();
-    my $sql = "select go_term_id, go_id
-               from sres.goterm
-               where external_database_release_id = $goExtDb";
-    
-    my $sth = $queryHandle->prepareAndExecute($sql);
-
-    my $gusGoMap;
-
-    while (my ($gusId, $realId) = $sth->fetchrow_array()){
-	$gusGoMap->{$gusId} = $realId;
-    }
-    return $gusGoMap;
+    return $msg;
 }
 
 
@@ -112,7 +139,7 @@ sub _getProteinGoMap{
 
     my $queryHandle = $self->getQueryHandle();
     my $sql = "select ga.row_id, ga.go_term_id
-               from DoTS.GOAssociation ga, DoTS.GOTerm gt, 
+               from DoTS.GOAssociation ga, SRes.GOTerm gt 
                where ga.table_id = $proteinTableId
                and ga.go_term_id = gt.go_term_id
                and gt.external_database_release_id = $goExtDb
@@ -130,13 +157,32 @@ sub _getProteinGoMap{
     return $proteinGoMap;
 }
 
+sub _makeGoMap{
+
+    my ($self) = @_;
+
+    my $queryHandle = $self->getQueryHandle();
+    
+    my $sql = "select go_term_id, go_id
+               from sres.goterm
+               where external_database_release_id = $goExtDb";
+    
+    my $sth = $queryHandle->prepareAndExecute($sql);
+
+    my $gusGoMap;
+
+    while (my ($gusId, $realId) = $sth->fetchrow_array()){
+	$gusGoMap->{$gusId} = $realId;
+    }
+    return $gusGoMap;
+}
 
 
 sub _makeGoAssociation{
 
     my ($self, $proteinId, $goTermId, $realGoId) = @_;
     
-    my $assoc = GUS::Model::DoTS::GoAssociation->new();
+    my $assoc = GUS::Model::DoTS::GOAssociation->new();
     $assoc->setTableId($proteinTableId);
     $assoc->setRowId($proteinId);
     $assoc->setGoTermId($goTermId);
@@ -144,7 +190,7 @@ sub _makeGoAssociation{
     $assoc->setDefining(0);
     $assoc->setReviewStatusId(1);
 
-    my $instance = GUS::Model::DoTS::GoAssociationInstance->new();
+    my $instance = GUS::Model::DoTS::GOAssociationInstance->new();
     $instance->setGoAssocInstLoeId($curatorLoeId);
     $instance->setExternalDatabaseReleaseId($goExtDb);
     $instance->setIsNot(1);
@@ -152,10 +198,64 @@ sub _makeGoAssociation{
     $instance->setIsDeprecated(0);
     $instance->setReviewStatusId(1);
 
-    print $output "protein id:\t$proteinId\tgo term id:\t$realGoId\n";
+    print $output "protein id:\t$proteinId\tgo term id:\t$realGoId\tgo term gus id: $goTermId\n";
 
     $assoc->addChild($instance);
 
     $assoc->submit();
+
+    $self->undefPointerCache();
+}
+
+
+sub _setAllInstancesToPrimary{
+
+    my ($self) = @_;
+
+    my $queryHandle = $self->getQueryHandle();
+    my $sql = "update DoTS.GOAssociationInstance gai, DoTS.GOAssociation ga
+               set gai.is_primary = 1
+               where ga.go_association_id = gai.go_association_id
+               and ga.row_id = 180";
+
+    my $sth = $queryHandle->prepareAndExecute($sql);
+
+    return "set is_primary = 1 for all instances of associations to proteins"; 
+
+}
+
+sub _deprecateAutomatedInstances{
+
+    my ($self) = @_;
+
+    my $queryHandle = $self->getQueryHandle();
+    my $sql = "update DoTS.GOAssociationInstance gai, DoTS.GOAssociation ga
+               set gai.is_deprecated = 1
+               where ga.go_association_id = gai.go_association_id
+               and ga.row_id = 180
+               and gai.review_status_id != 1"
+	      ;
+
+    my $sth = $queryHandle->prepareAndExecute($sql);
+
+    return "set is_deprecated = 1 for all non-manually reviewed instances of associations to proteins"; 
+
+}
+
+sub _deprecateOldInstances{
+
+    my ($self) = @_;
+
+    my $queryHandle = $self->getQueryHandle();
+    my $sql = "update DoTS.GOAssociationInstance gai, DoTS.GOAssociation ga, DoTS.GOTerm gt
+               set gai.is_deprecated = 1
+               where ga.go_association_id = gai.go_association_id
+               and ga.go_term_id = gt.go_term_id
+               and ga.row_id = 180
+               and gt.external_database_release_id != $goExtDb";
+
+    my $sth = $queryHandle->prepareAndExecute($sql);
+
+    return "set is_deprecated = 1 for all non-manually reviewed instances of associations to proteins"; 
 
 }
