c0a46370bd7a8441d804eadafcab1cdecca25142
Steve Fischer <sfischer@upenn.edu>
2003-08-18 13:59:20 +0000

    *** empty log message ***
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/GUS/trunk@1504 4235af6a-31f9-0310-9ad3-ecf6348f2534

---
+++ Website/htdocs/plugins/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html public "-//w3c//dtd html 4.0 transitional//en">
+<HTML>
+<HEAD>
+   <META http-equiv="Content-Type" content="text/html;
+charset=iso-8859-1">
+    <meta name="Content-Type" content="text/html; charset=iso-8859-1">
+<TITLE> GUSDB doc page </TITLE></HEAD>
+
+<BODY>
+<h1>GUS Plugins</h1>
+<BR><HR><BR>
+<?php
+        $currdir = opendir('.');
+        while ($f = readdir($currdir)) {
+            if (ereg("^\.+",$f)) {
+               # do nothing
+            } else if (ereg("^index",$f)) {
+            } else {
+                  print "<a href='$f'><BR>$f<BR>\n";
+            }
+    }
+        closedir($currdir);
+?>
+
+</BODY></HTML>
+
+
+
