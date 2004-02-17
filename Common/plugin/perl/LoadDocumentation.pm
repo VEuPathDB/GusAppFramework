449e0ac57417b073151572bee16af1a79095d546
mmailman <mmailman@_.com>
2004-02-17 20:31:20 +0000

    *** empty log message ***
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/GUS/trunk@1852 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Common/plugin/perl/LoadDocumentation.pm
+++ Common/plugin/perl/LoadDocumentation.pm
@@ -141,8 +141,9 @@ sub process {
 			$tb_id = $ary[0]; #queried table id
 			$att_name = $ary[1]; #queried attribute name
 			$html = $ary[2]; #queried html documentation
-			if ($html eq $html_dc){ 
-#			if ($html =~ /\$html_dc/){  #######????????????############
+			$self->logAlert("Comparing html: $html\nwith html_dc: $html_dc\n");
+			
+			if ($html eq $html_dc){ #########?????????############
 			    $self->logAlert("ALREADY EXISTS! Documentation for column $table_nm" .
 					    "." ."$attribute_nm NOT OVERWRITTEN!");
 			    return; # SKIP
