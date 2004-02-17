7a0086441399c3501793adf4f28a3c2059bee0ff
Angel Pizarro <delagoya@gmail.com>
2004-02-17 19:18:58 +0000

    Updating dumpSchema date for GUS 3.2
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/GUS/trunk@1845 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Model/schema/oracle/sresver-pkey-constraints.sql
+++ Model/schema/oracle/sresver-pkey-constraints.sql
@@ -2,7 +2,7 @@
 /*                                                                                            */
 /* sresver-pkey-constraints.sql                                                               */
 /*                                                                                            */
-/* This file was generated automatically by dumpSchema.pl on Tue Dec  9 16:10:23 EST 2003     */
+/* This file was generated automatically by dumpSchema.pl on Tue Feb 17 12:47:44 EST 2004     */
 /*                                                                                            */
 
 SET ECHO ON
