74a9bfd0b1bb5bb070b9ec396f1241f43c258c40
mmailman <mmailman@_.com>
2004-02-17 21:37:27 +0000

    *** empty log message ***
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/GUS/trunk@1863 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Common/plugin/perl/LoadDocumentation.pm
+++ Common/plugin/perl/LoadDocumentation.pm
@@ -142,6 +142,8 @@ sub process {
 		    $stmt->execute();
 		    my ($tb_id, $att_name, $html);
 
+		    $self->logAlert("tb_id = $tb_id\tatt_name = $att_name\thtml = $html\n"); ###TEST###
+
 		    while (my @ary = $stmt->fetchrow_array() ){
 			chomp;
 			$tb_id = $ary[0]; #queried table id
@@ -207,6 +209,8 @@ sub process {
 		my $stmt2 = $dbh2->prepare($query2);
 		$stmt2->execute();
 		my ($tb_id2, $html2);
+
+		$self->logAlert("tb_id2 = $tb_id2\thtml2 = $html2\n"); ###TEST###
 
 		while (my @ary2 = $stmt2->fetchrow_array() ){
 		    chomp;
