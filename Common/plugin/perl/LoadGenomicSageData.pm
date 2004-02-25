bdbec5c9fb00fa7c0d5d2724a8308b7ca2cef197
John Iodice <iodice@pennmedicine.upenn.edu>
2004-02-25 16:09:01 +0000

    *** empty log message ***
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/GUS/trunk@1878 4235af6a-31f9-0310-9ad3-ecf6348f2534

---
+++ Common/plugin/perl/LoadGenomicSageData.pm
@@ -0,0 +1,311 @@
+package GUS::Common::Plugin::LoadGenomicSageData;
+@ISA = qw(GUS::PluginMgr::Plugin);
+
+use strict;
+use GUS::PluginMgr::Plugin;
+use GUS::Model::DoTS::SAGETagFeature;
+use GUS::Model::DoTS::GeneFeature;
+use GUS::Model::DoTS::GeneFeatureSAGETagLink;
+use GUS::Model::DoTS::NALocation;
+use GUS::Model::DoTS::ExternalNASequence;
+use GUS::Model::RAD3::SAGETag;
+use GUS::Model::RAD3::SAGETagMapping;
+use GUS::Model::Core::DatabaseInfo;
+use GUS::Model::Core::TableInfo;
+
+sub new {
+  my ($class) = @_;
+  my $self = {};
+  bless($self,$class);
+
+  my $purposeBrief = "Generate SAGE data from sequences and genes.";
+
+  my $purpose = <<PLUGIN_PURPOSE;
+This plugin generates SAGE data from genomic data.  Specifically, it finds SAGE
+tags in ExternalNaSequences, and creates GeneFeatureSageTagLinks to associate
+SAGE tags with genes.  These associations are made by the findLinks method.
+This method, written by Jonathan Crabtree for an earlier version of this
+plugin, links each SAGE tag to any genes found within a given distance (by
+default, 1000 bp).
+PLUGIN_PURPOSE
+
+  my $tablesAffected =
+    [ ['DoTS::GeneFeatureSAGETagLink',
+       'Insert a record for each predicted link'],
+      ['DoTS::SAGETagFeature',
+       'Insert a record for each predicted SAGE tag feature'],
+      ['DoTS::NALocation', 'Insert three records for each predicted tag'],
+      ['RAD3::SAGETag', 'Insert a record for any novel tag sequence'],
+      ['RAD3::SAGETagMapping',
+       'Map DoTS sourceId-ExtDbRelId to RAD CompositeElementId'],
+    ];
+
+  my $tablesDependedOn =
+    [ ['DoTS::ProjectLink', 'Identify sequences to scan for tags'],
+      ['DoTS::ExternalNASequence',
+       'Get sequences to scan for SAGE tag features'],
+      ['DoTS::SAGETagFeature', 'Get SAGE tag features to link'],
+      ['DoTS::GeneFeature', 'Get gene features to link'],
+      ['DoTS::NALocation',
+       'Get locations within ExternalNaSequence of SAGE tags and genes'],
+      ['Core::DatabaseInfo', 'Look up table IDs by database/table names'],
+      ['Core::TableInfo', 'Look up table IDs by database/table names'],
+    ];
+
+  my $howToRestart = <<PLUGIN_RESTART;
+This plugin has no restart facility.
+PLUGIN_RESTART
+
+  my $failureCases = <<PLUGIN_FAILURE_CASES;
+PLUGIN_FAILURE_CASES
+
+  my $notes = <<PLUGIN_NOTES;
+PLUGIN_NOTES
+
+  my $documentation = { purpose=>$purpose,
+                        purposeBrief=>$purposeBrief,
+                        tablesAffected=>$tablesAffected,
+                        tablesDependedOn=>$tablesDependedOn,
+                        howToRestart=>$howToRestart,
+                        failureCases=>$failureCases,
+                        notes=>$notes
+                      };
+
+  my $argsDeclaration =
+    [
+     integerArg({name  => 'project_id',
+		 descr => 'project_id',
+		 reqd  => 1,
+		 constraintFunc=> undef,
+		 isList=> 0,
+		}),
+     integerArg({name  => 'taxon_id',
+		 descr => 'taxon_id',
+		 reqd  => 1,
+		 constraintFunc=> undef,
+		 isList=> 0,
+		}),
+     integerArg({name  => 'array_id',
+		 descr => 'array_id',
+		 reqd  => 0,
+		 constraintFunc=> undef,
+		 isList=>0,
+		}),
+     integerArg({name  => 'max_distance',
+		 descr => 'max_distance',
+		 reqd  => 0,
+		 default  => 1000,
+		 constraintFunc=> undef,
+		 isList=> 0,
+		}),
+     booleanArg({name  => 'find_tags',
+		 descr => 'find_tags',
+		 reqd  => 0,
+		 constraintFunc=> undef,
+		 isList=> 0,
+		}),
+     booleanArg({name  => 'find_links',
+		 descr => 'find_links',
+		 reqd  => 0,
+		 constraintFunc=> undef,
+		 isList=> 0,
+		}),
+    ];
+
+
+  $self->initialize({requiredDbVersion => {},
+                     cvsRevision => '$Revision$', # cvs fills this in!
+                     cvsTag => '$Name$', # cvs fills this in!
+                     name => ref($self),
+                     revisionNotes => 'make consistent with GUS 3.0',
+                     argsDeclaration => $argsDeclaration,
+                     documentation => $documentation
+                    });
+  return $self;
+}
+
+sub run {
+  my ($self) = @_;
+  $self->logAlgInvocationId;
+  $self->logCommit;
+
+  # validate command-line arguments
+  if (!($self->getArg('find_tags') || $self->getArt('find_links'))) {
+    $self->userError('--find_tags or --find_links must be specified');
+  }
+
+  $self->processSequencesByProjectId($self->getArg('project_id'),
+				  $self->getArg('taxon_id'));
+}
+
+# Given a projectId, use ProjectLink to find ExternalNaSequences
+# to scan for SageTagFeatures and GeneFeatures, creating links
+# where appropriate
+
+sub processSequencesByProjectId {
+  my ($self, $project_id, $taxon_id) = @_;
+
+  # outside loop, prepare query to run against every na_sequence_id
+  my $queryHandle = $self->prepareQuery($self->getArg('max_distance'));
+
+  my $table_id = getTableId('DoTS', 'ExternalNASequence');
+
+  my $sql = <<SQL;
+    select pl.id
+    from DoTS.ProjectLink pl, DoTS.ExternalNaSequence ens
+    where pl.project_id = $project_id
+      and pl.table_id = $table_id
+      and pl.id = ens.na_sequence_id
+      and ens.taxon_id = $taxon_id
+SQL
+
+  my $dbh = $self->getQueryHandle();
+  my $stmt = $dbh->prepareAndExecute($sql);
+
+  my $linksFound = 0;
+  while (my ($na_sequence_id) = $stmt->fetchrow_array()) {
+
+    $self->logVerbose("processing NaSequenceId $na_sequence_id");
+    $linksFound += $self->findLinks($na_sequence_id, $queryHandle);
+    $self->undefPointerCache();
+  }
+}
+# ----------------------------------------------------------------------
+sub prepareQuery {
+  my ($self, $maxDistance) = @_;
+
+  # This join gets ids and locations for close pairs of features.
+  # The ugly expression that gets selected as "same_strand" should be
+  # 1 if both records have the same value in "is_reversed", 0 otherwise.
+
+  my $sql = <<SQL;
+        select tf.na_feature_id as tag_id,
+               gf.na_feature_id as gene_id,
+               tfl.start_min,
+               tfl.end_max,
+               tfl.is_reversed,
+	       gfl.start_min,
+               gfl.end_max,
+               gfl.is_reversed,
+               (nvl(tfl.is_reversed, 0) * nvl(gfl.is_reversed, 0)
+                + (1 - nvl(tfl.is_reversed, 0) )
+                   * (1 - nvl(gfl.is_reversed, 0)))
+                 as same_strand
+	from DoTS.SAGETagFeature tf,
+             DoTS.NALocation tfl,
+	     DoTS.GeneFeature gf,
+             DoTS.NALocation gfl
+	where gf.na_sequence_id = ?
+	and tf.na_sequence_id = gf.na_sequence_id
+	and tf.na_feature_id = tfl.na_feature_id
+	and gf.na_feature_id = gfl.na_feature_id
+	and (abs(gfl.end_min - tfl.start_max) <= $maxDistance
+	     or abs(gfl.start_max - tfl.end_min) <= $maxDistance
+	     or (gfl.start_max <= tfl.end_min and gfl.end_min >= tfl.start_max))
+SQL
+
+print STDERR "$sql\n";
+
+  my $dbh = $self->getQueryHandle();
+  my $stmt = $dbh->prepare($sql);
+
+  return $stmt;
+}
+
+# ----------------------------------------------------------------------
+# Given an na_sequence_id, use SQL to find linked gene/SAGE tag pairs.
+# This is just a copy of the _storeLinks method from the old version
+# of this plugin, modified to work with GUS v. 3 objects.
+
+sub findLinks {
+  my ($self, $na_sequence_id, $queryHandle) = @_;
+
+  $queryHandle->execute($na_sequence_id);
+
+  my $nLinks = 0;
+
+  while (my($tagId,$geneId,$ts,$te,$trev,$gs,$ge,$grev,$ss)
+	 = $queryHandle->fetchrow_array()) {
+    my($tt5p, $tt3p);
+
+    if (!$grev) {
+      if (($ts <= $gs) && ($te >= $gs)) {
+	$tt5p = 0;
+      } else {
+	$tt5p = ($te <= $gs) ? $gs - $te : $gs - $ts;
+      }
+      if (($ts <= $gs) && ($te >= $gs)) {
+	$tt3p = 0;
+      } else {
+	$tt3p = ($ts >= $ge) ? $ts - $ge : $te - $ge;
+      }
+    } else {
+      if (($ts <= $ge) && ($te >= $ge)) {
+	$tt5p = 0;
+      } else {
+	$tt5p = ($ts >= $ge) ? $ts - $ge : $te - $ge;
+      }
+      if (($ts <= $ge) && ($te >= $ge)) {
+	$tt3p = 0;
+      } else {
+	$tt3p = ($te <= $gs) ? $gs - $te : $gs - $ts;
+      }
+    }
+
+    my $geneFeatureSageTagLink =
+      GUS::Model::DoTS::GeneFeatureSAGETagLink->new
+	  ( {
+	     genomic_na_sequence_id => $na_sequence_id,
+	     gene_na_feature_id => $geneId,
+	     tag_na_feature_id => $tagId,
+	     five_prime_tag_offset => $tt5p,
+	     three_prime_tag_offset => $tt3p,
+	     same_strand => $ss,
+	     experimentally_verified => 0
+	    } );
+
+    if ($geneFeatureSageTagLink->retrieveFromDB()) {
+      die("SageTagLink already exists between geneFeature " . $geneId .
+	  " and sageTagFeature " . $tagId);
+    }
+    $geneFeatureSageTagLink->submit();
+    $self->undefPointerCache();
+
+    ++$nLinks;
+  }
+  $queryHandle->finish();
+  return $nLinks;
+}
+
+# ======================================================================
+# ----------------------------------------------------------------------
+# ======================================================================
+
+# look up tableId by database/table name
+sub getTableId {
+  my ($dbName, $tableName) = @_;
+
+  my $db =
+    GUS::Model::Core::DatabaseInfo->new( { name => $dbName } );
+  $db->retrieveFromDB();
+
+  my $table =
+    GUS::Model::Core::TableInfo->new
+	( {
+	   name => $tableName,
+	   database_id => $db->getDatabaseId()
+	  } );
+
+  $table->retrieveFromDB();
+
+  my $id = $table->getTableId();
+
+  if (! $id ) {
+    die("can't get tableId for '$dbName\.$tableName'");
+  }
+
+  return $id;
+
+}
+
+1;
