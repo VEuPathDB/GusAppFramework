6a1ccd8fedae721a6d8268dace2c0f11539e9246
svdate <svdate@_.com>
2004-11-03 21:06:43 +0000

    - Documentation changes
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/GUS/trunk@2262 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- RAD/plugin/perl/GenePixStudyModuleILoader.pm
+++ RAD/plugin/perl/GenePixStudyModuleILoader.pm
@@ -38,8 +38,8 @@ sub new {
     ['RAD3::Study',                 'One row for the given study is entered'],
     ['RAD3::Assay',                 'One row for each distinct assay is entered'],
     ['RAD3::StudyAssay',            'One row linking the given study to each assay is entered'],
-    ['RAD3::Acquisition',           'Two rows, two for each channel (Cy5 and Cy3), corresponding to one assay, are entered'],
-    ['RAD3::Quantification',        'Two rows, two for each channel (Cy5 and Cy3), corresponding to one acquisition, are entered'],
+    ['RAD3::Acquisition',           'Two rows, one for each channel (Cy5 and Cy3), corresponding to one assay, are entered'],
+    ['RAD3::Quantification',        'Two rows, one for each channel (Cy5 and Cy3), corresponding to one acquisition, are entered'],
     ['RAD3::QuantificationParam',   'Eight rows, four for each channel (Cy5 and Cy3), corresponding to four parameters per quantification, are entered'],
     ['RAD3::RelatedAcquisition',    'Two rows, associating two channels (Cy5 and Cy3) with each other, are entered'],
     ['RAD3::RelatedQuantification', 'Two rows, associating two channels (Cy5 and Cy3) with each other, are entered']
