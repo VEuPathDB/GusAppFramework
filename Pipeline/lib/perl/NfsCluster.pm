44ac75f7dc719ab01f8cd35ac8d4290b825c1a81
Mark Heiges <mheiges@gmail.com>
2005-02-23 03:49:45 +0000

    corrected copyTo runCmd and replaced cp with tar
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/GUS/trunk@2438 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Pipeline/lib/perl/NfsCluster.pm
+++ Pipeline/lib/perl/NfsCluster.pm
@@ -28,12 +28,16 @@ sub setManager {
 #  param fromFile - the basename of the file or directory to copy
 sub copyTo {
     my ($self, $fromDir, $fromFile, $toDir) = @_;
+        # buildDir, release/speciesNickname, serverPath
+        
+    chdir $fromDir || $self->{mgr}->error("Can't chdir $fromDir\n" . __FILE__ . " line " . __LINE__ . "\n\n");
 
-    $self->{mgr}->error("origin file or directory $fromDir/$fromFile doesn't exist\n") unless -e "$fromDir/$fromFile";
-    $self->{mgr}->error("destination directory $toDir doesn't exist\n") unless -d $toDir;
-
-    $self->{mgr}->runCmd("cp $fromDir/$fromFile $toDir");
+    $self->{mgr}->error("origin file or directory $fromDir/$fromFile doesn't exist\n" . __FILE__ . " line " . __LINE__ . "\n\n") unless -e "$fromDir/$fromFile";
+    $self->{mgr}->error("destination directory $toDir doesn't exist\n" . __FILE__ . " line " . __LINE__ . "\n\n") unless -d $toDir;
+    
+    $self->{mgr}->runCmd("tar cf - $fromFile | (cd $toDir &&  tar xBf -)");
 }
+
 
 #  param fromDir  - the directory in which fromFile resides
 #  param fromFile - the basename of the file or directory to copy
