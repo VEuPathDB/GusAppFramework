21892c92dd32df1656d6acd5ee921c2b6af8abdd
Deborah Pinney <pinney@upenn.edu>
2005-03-08 18:17:51 +0000

    new plugin
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/GUS/trunk@2448 4235af6a-31f9-0310-9ad3-ecf6348f2534

---
+++ Common/plugin/perl/DeleteFromTable.pm
@@ -0,0 +1,113 @@
+package GUS::Common::Plugin::DeleteFromTable;
+
+@ISA = qw(GUS::PluginMgr::Plugin);
+
+use strict;
+
+
+
+$| = 1;
+
+sub new {
+  my $class = shift;
+  my $self = bless {}, $class;
+
+  my $usage = 'Plug_in to delete rows from given table and its children';
+
+  my $easycsp =
+    [{o => 'testnumber',
+      t => 'int',
+      h => 'number of iterations for testing',
+     },
+     {o => 'idSQL',
+      t => 'string',
+      h => 'query used to get ids of rows to delete',
+     },
+     {o => 'table',
+      t => 'string',
+      h => 'table from which to delete, in standarg form e.g. DoTS::ExternalNASequence ',
+     },
+     {o => 'delete_children',
+      t => 'boolean',
+      h => 'explicit option to delete children, used as a safeguard',
+     }
+     ];
+
+ $self->initialize({requiredDbVersion => {},
+                  cvsRevision => '$Revision$', # cvs fills this in!
+                  cvsTag => '$Name$', # cvs fills this in!
+                  name => ref($self),
+                  revisionNotes => 'make consistent with GUS 3.0',
+                  easyCspOptions => $easycsp,
+                  usage => $usage
+                 });
+  return $self;
+}
+
+sub run {
+  my $self  = shift;
+  $self->getArgs()->{'commit'} ? $self->log("***COMMIT ON***\n") : $self->log("**COMMIT TURNED OFF**\n");
+  $self->log("Testing on " . $self->getArgs()->{'testnumber'}) if $self->getArgs()->{'testnumber'};
+
+  die "Supply sql and table\n $usage\n " unless ($self->getArgs()->{'table'} && $self->getArgs()->{'idSQL'});
+
+  my $ids = $self->getIdsToDelete();
+  my $results = $self->deleteRows($ids);
+  return $results;
+
+}
+
+
+sub getIdsToDelete { 
+  my ($self) = @_;
+
+  my @ids;
+  $self->log("Getting ids to delete");
+
+  my $dbh = $self->getDb()->getDbHandle();
+  my $sql = ($self->getArgs()->{'idSQL'});
+  $sql .= " where" if ($self->getArgs()->{'testnumber'} && $sql !~ /where/);
+  $sql .= " and rownum = " . $self->getArgs()->{'testnumber'} if $self->getArgs()->{'testnumber'};
+
+  my $stmt = $dbh->prepareAndExecute($sql) || die "SQL failed: $sql\n";
+
+  while (my ($id) = $stmt->fetchrow_array) {
+    push (@ids, $id);
+  }
+
+  $stmt->finish;
+
+  my $num =  @ids;
+  $self->log("Got $num ids to delete\n using: $sql\n");
+
+  return /@ids;
+}
+
+sub deleteRows {
+  my ($self, $ids) = @_;
+  my $numDelete = 0;
+
+  my $prim_key = $self->getTablePKFromTableId($self->getTableIdFromTableName($self->getArgs()->{'table'}));
+
+  my $table = $self->getArgs()->{'table'};
+
+  foreach my $id (@$ids) {
+    my $newTable = GUS::Model::$table->new ({'$prim_key'=>$id});
+    $newTable->retrieveFromDB();
+    $newTable->retrieveAllChildrenFromDB(1) if ($self->getArgs()->{'delete_children'});
+    if ($self->getArgs()->{'delete_children'}) {
+      $newTable->markDeleted(1);
+    }
+    else {
+      $newTable->markDeleted(1);
+    }
+    my $n = $newTable->submit();
+    $newTable->undefPointerCache();
+    $numDelete++ if $n >= 1;
+  }
+  my $results = "Number of rows deleted from $table : $numDelete\n";
+  return $results;
+}
+
+1;
+__END__
