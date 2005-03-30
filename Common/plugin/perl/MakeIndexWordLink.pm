c2ace9f9cd9d05b087a82819a1485eada34cb1ef
Bindu Gajria <bindu.gajria@gmail.com>
2005-03-30 18:06:45 +0000

    $M to call getDb
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/GUS/trunk@2495 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Common/plugin/perl/MakeIndexWordLink.pm
+++ Common/plugin/perl/MakeIndexWordLink.pm
@@ -147,11 +147,11 @@ and rownum < 100";
       $words{$wd} = 1;
     }
 
-    $ctx->{self_inv}->getDb()->manageTransaction(0,'begin');
+    $M->getDb()->manageTransaction(0,'begin');
     foreach my $wd (keys%words){
       &processWord($id,$wd);
     }
-    $ctx->{self_inv}->getDb()->manageTransaction(0,'commit');
+    $M->getDb()->manageTransaction(0,'commit');
 
     $ctx->{self_inv}->undefPointerCache();
   }
