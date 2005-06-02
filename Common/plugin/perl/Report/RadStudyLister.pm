f5748613688e24e06a8c1e06bf5721a600584c9b
Jonathan Schug <jschug@upenn.edu>
2005-06-02 21:11:46 +0000

    Initial entry of read-only access reports.
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/GUS/trunk@2598 4235af6a-31f9-0310-9ad3-ecf6348f2534

---
+++ Common/plugin/perl/Report/RadStudyLister.pm
@@ -0,0 +1,111 @@
+
+package GUS::Common::Plugin::Report::RadStudyLister;
+@ISA = qw(GUS::PluginMgr::Plugin);
+
+# ----------------------------------------------------------------------
+
+use strict;
+
+use FileHandle;
+use V;
+
+use GUS::PluginMgr::Plugin;
+
+use GUS::PluginMgr::PluginUtilities::ConstraintFunction;
+use GUS::PluginMgr::PluginUtilities;
+
+use GUS::Model::RAD3::Study;
+
+# ======================================================================
+
+sub new {
+   my $Class = shift;
+
+   my $self = bless {}, $Class;
+
+   my $selfPodCommand = 'pod2text '. __FILE__;
+   my $selfPod        = `$selfPodCommand`;
+
+   $self->initialize
+   ({ requiredDbVersion => {},
+      cvsRevision       => ' $Revision$ ',
+      cvsTag            => ' $Name$ ',
+      name              => ref($self),
+
+      # just expand
+      revisionNotes     => '',
+      revisionNotes     => 'initial creation and testing',
+
+      # ARGUMENTS
+      argsDeclaration   =>
+      [
+      ],
+
+      # DOCUMENTATION
+      documentation     =>
+      { purpose          => <<Purpose,
+
+  This plugin lists RAD3.Studies and dependent info
+
+Purpose
+        purposeBrief     => 'List RAD3.Studies',
+        tablesAffected   =>
+        [ ],
+        tablesDependedOn =>
+        [
+        ],
+        howToRestart     => 'just restart; this is a reader',
+        failureCases     => 'just run again',
+        notes            => $selfPod,
+      },
+    });
+
+   # RETURN
+   $self
+}
+
+# ======================================================================
+
+sub run {
+   my $Self       = shift;
+
+	 $Self->logArgs();
+
+   my $sql = 'select max(length(name)) from SRes.ExternalDatabase';
+   my ($w) = @{$Self->sql_get_as_array($sql)};
+   my $name_fmt = "%-${w}.${w}s";
+
+   my $sql = 'select max(length(version)) from SRes.ExternalDatabaseRelease';
+   my ($w) = @{$Self->sql_get_as_array($sql)};
+   my $version_fmt = "%-${w}.${w}s";
+
+   my $sql = 'select study_id from RAD3.Study order by name';
+   my @study_ids = @{$Self->sql_get_as_array($sql)};
+
+   for (my $i = 0; $i < @study_ids; $i += 2 ) {
+      my $study_id = $study_ids[$i];
+
+      my $_study = GUS::Model::RAD3::Study->new
+      ({ study_id => $study_id });
+      $_study->retrieveFromDB();
+
+#      foreach my $_studyr (sort { $a->getId() <=> $b->getId() }
+#                         $_study->getChildren('SRes::ExternalDatabaseRelease',1)
+#                        ) {
+#
+      $Self->log('STUDY',
+                 $_study->getId()           || 'study.id?',
+                 sprintf($name_fmt,    $_study->getName() || 'Study.name?'),
+                 sprintf($description_fmt,    $_study->getDescription() || 'Study.description?'),
+#
+#                    $_studyr->getId()          || 'Studyr.id?',
+#                    sprintf($version_fmt, $_studyr->getVersion()     || 'Studyr.ver?'),
+#                    sprintf('%-24.24s',   $_studyr->getReleaseDate() || 'Studyr.rd?'),
+#                    $_studyr->getDescription() || 'Studyr.descr?'
+                   );
+   }
+}
+
+# ----------------------------------------------------------------------
+
+1;
