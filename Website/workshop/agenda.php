d7e875d44f161437012c9fc640984c0527731f70
Michael Saffitz <m@saffitz.com>
2005-06-22 15:33:52 +0000

    added workshop pages
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/GUS/branches/testing-3.5@3008 4235af6a-31f9-0310-9ad3-ecf6348f2534

---
+++ Website/workshop/agenda.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+       "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+<title>:: GUS Workshop :: </title>
+<link href="/workshop/screen.css" media="screen" rel="Stylesheet" type="text/css" />
+<meta http-equiv="Content-Language" content="en">
+<meta name="ROBOTS" content="ALL">
+<meta http-equiv="imagetoolbar" content="no">
+</head>
+
+<body>
+
+
+<div id="Page">
+
+<div class="title">
+<h1>The Genomics Unified Schema</h1>
+<h2>User's and Developer's Workshop</h2>
+<h3>July 6-8, 2005 -- Philadelphia, Pennsylvania</h3>
+</div>
+
+<div class="menu">
+<span class="menuItem"><a href="index.php">About</a></span>
+<span class="menuItem"><a href="agenda.php">Agenda</a></span>
+<span class="menuItem"><a href="registration.php">Registration</a></span>
+</div>
+
+<div class="body">
+
+<h3>Workshop Agenda</h3>
+
+<p>Abstracts coming soon...</p>
+
+<strong>Wednesday, July 6 -- GUS Overview & Setting up GUS</strong>
+<table border="1" style="border-style: solid; border-collapse: collapse;" cellpadding="3" width="100%">
+<tr><th>Start Time</th><th>Title</th><th>Speaker</th></tr>
+<tr><th>8:30am</th><td>Registration and Breakfast</td><td></td></tr>
+<tr><th>9:00am</th><td>Welcome, GUS History, Status, and Vision</td><td>Chris Stoeckert & Mike Saffitz, CBIL</td></tr>
+<tr><th>10:15am</th><td>Break</td><td></td></tr>
+<tr><th>10:30am</th><td>Building CryptoDB using GUS</td><td>Mark Heiges, Kissinger Lab, UGA</td></tr>
+<tr><th>11:30am</th><td>GUS Introduction Q&A and Development Priorities</td><td>Discussion</td></tr>
+<tr><th>12:00pm</th><td>Lunch</td><td></td></tr>
+<tr><th>1:00pm</th><td>GUS Development Priorities, Cont.</td><td>Discussion</td></tr>
+<tr><th>3:00pm</th><td>GUS Implementation Workshop: Getting 3.5 up and running</td><td>Workshop</td></tr>
+</table>
+<p/>
+<strong>Thursday, July 7 -- Data Loading</strong>
+<table border="1" style="border-style: solid; border-collapse: collapse;" cellpadding="3" width="100%" >
+<tr><th>Start Time</th><th>Title</th><th>Speaker</th></tr>
+<tr><th>8:30am</th><td>Breakfast</td><td></td></tr>
+<tr><th>9:00am</th><td>GUS Object Layer & Pipeline</td><td>Debbie Pinney, CBIL</td></tr>
+<tr><th>10:00am</th><td>Break</td><td></td></tr>
+<tr><th>10:15am</th><td>XML GUS Data Loading Tools</td><td>Josef Jurek, U. Chicago</td></tr>
+<tr><th>11:15am</th><td>Writing and using a GUS Plug-in</td><td>TBA</td></tr>
+<tr><th>12:00pm</th><td>Lunch</td><td></td></tr>
+<tr><th>1:00pm</th><td><strong>GUS Implementation Workshop:</strong><br/>Loading Data with Plug-ins<br/>
+			Loading Data using a pipeline<br/>Writing a Plug-in</td><td>Workshop</td></tr>
+</table>
+<p/>
+<strong>Friday, July 8 -- Web Applications and Tools for GUS</strong>
+<table border="1" style="border-style: solid; border-collapse: collapse;" cellpadding="3" width="100%">
+<tr><th>Start Time</th><th>Title</th><th>Speaker</th></tr>
+<tr><th>8:30am</th><td>Breakfast</td><td></td></tr>
+<tr><th>9:00am</th><td>The Web Development Kit (WDK)</td><td>Thomas Gan, CBIL</td></tr>
+<tr><th>10:00am</th><td>Break</td><td></td></tr>
+<tr><th>10:15am</th><td>GUS GBrowse Integration</td><td>Haiming Wang, Kissinger Lab, UGA</td></tr>
+<tr><th>11:15am</th><td>Web and Tool Integration Architectures</td><td>Carey Gire, NGC</td></tr>
+<tr><th>12:15pm</th><td>Lunch</td><td></td></tr>
+<tr><th>1:15pm</th><td><strong>GUS Implementation Workshop:</strong><br/>Setting up the WDK<br/>
+				Coordination of Tool Adapters</td><td>Workshop</td></tr>
+<tr><th>4:00pm</th><td>GUS Workshop Wrap Up: Future Plans and Feedback</td><td>Discussion</td></tr>
+</table>
+
+
+
+</div>
+
+</div>
+
+</body>
+
+</html>
