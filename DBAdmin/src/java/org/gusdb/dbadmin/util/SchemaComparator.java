b81ed9a552f88319882b7db662f8d3b45f4f6490
Michael Saffitz <m@saffitz.com>
2005-07-13 21:33:11 +0000

    support for comparing schemas, partially implemented code
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/GUS/trunk@3096 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- DBAdmin/src/java/org/gusdb/dbadmin/util/SchemaComparator.java
+++ DBAdmin/src/java/org/gusdb/dbadmin/util/SchemaComparator.java
@@ -1,141 +1,232 @@
-/*
- * Created on 2/25/05
- *
+/**
+ * 
  */
 package org.gusdb.dbadmin.util;
 
-import org.gusdb.dbadmin.model.Database;
-import org.gusdb.dbadmin.reader.SchemaReader;
-import org.gusdb.dbadmin.reader.XMLReader;
-import org.gusdb.dbadmin.reader.OracleReader;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Vector;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-
-import org.apache.commons.cli.BasicParser;
-import org.apache.commons.cli.CommandLine;
-import org.apache.commons.cli.CommandLineParser;
-import org.apache.commons.cli.HelpFormatter;
-import org.apache.commons.cli.Option;
-import org.apache.commons.cli.OptionGroup;
-import org.apache.commons.cli.Options;
-import org.apache.commons.cli.ParseException;
+import org.gusdb.dbadmin.model.Column;
+import org.gusdb.dbadmin.model.Database;
+import org.gusdb.dbadmin.model.GusTable;
+import org.gusdb.dbadmin.model.Schema;
+import org.gusdb.dbadmin.model.Table;
 
 /**
  * @author msaffitz
- * @version $Revision$ $Date$
  */
 public class SchemaComparator {
 
-    protected static Log log = LogFactory.getLog(SchemaComparator.class);
+    protected static final Log log                    = LogFactory
+                                                              .getLog( SchemaComparator.class );
 
-    public static void main(String[] args) {
-	SchemaReader schemaA;
-	SchemaReader schemaB;
-	
-	System.out.println("Has args: ");
-	for ( int i = 0; i < args.length; i++ ) {
-	    System.out.println(args[i]);
-	}
-	
-	String cmdName = System.getProperties().getProperty("cmdName");
-	
-	Options options = declareOptions();
-	CommandLine cmdLine = parseOptions(cmdName, options, args);
+    private Database           leftDatabase;
+    private Database           rightDatabase;
 
-	if ( cmdLine.hasOption("xmlA") ) {
-	    System.out.println("creating xml reader with " + cmdLine.getOptionValue("xmlA") );
-	    schemaA = new XMLReader( cmdLine.getOptionValue("xmlA") );
-	}
-	else {
-	    String[] params = cmdLine.getOptionValues("dbA");
-	    schemaA = new OracleReader( params[0], params[1], params[2] );
-	}
+    private Collection         leftIdenticalTables    = new Vector( );
 
-	if ( cmdLine.hasOption("xmlB") ) {
-	    schemaB = new XMLReader( cmdLine.getOptionValue("xmlB") );
-	}
-	else {
-	    String[] params = cmdLine.getOptionValues("dbB");
-	    schemaB = new OracleReader( params[0], params[1], params[2] );
-	}
-	
-	Database dbA = schemaA.read();
-	Database dbB = schemaB.read();
+    private HashMap            leftRenamedTables      = new HashMap( );
+    private HashMap            leftChangedTables      = new HashMap( );
+    private Collection         leftDroppedTables      = new Vector( );
 
-	if ( dbA.equals(dbB) ) {
-	    System.out.println("Databases are equal");
-	} else {
-	    System.out.println("Databases are not equal");
-	}
-	
-    }
-    
-    private static OptionGroup getSchemaSelection(String prefix) {
-	OptionGroup schemaSelection = new OptionGroup();
-	schemaSelection.setRequired(true);
+    private Collection         rightAddedTables;
 
-	Option xml = new Option("xml" + prefix, 
-		     "Use an XML file as the representation of the schema.");
-	schemaSelection.addOption(xml);
-	
-	Option db = new Option("db" + prefix,
-		     "Use a database instance for the schema.");
-	db.setArgs(3);
-	schemaSelection.addOption(db);
-	return schemaSelection;
+    private HashMap            potentialRenameMatches = new HashMap( );
+
+    public SchemaComparator( Database leftDatabase, Database rightDatabase ) {
+        this.leftDatabase = leftDatabase;
+        this.rightDatabase = rightDatabase;
     }
 
-    static Options declareOptions() {
-	Options options  = new Options();
+    public boolean compare( ) {
+        findLeftIdenticalTables( );
+        findLeftRenamedTables( );
+        findLeftChangedTables( );
+        findLeftDroppedTables( );
+        findRightAddedTables( );
 
-
-	options.addOptionGroup(getSchemaSelection("A"));
-	options.addOptionGroup(getSchemaSelection("B"));
-       
-	Option verbose = new Option("verbose", "Increase verbosity");
-	options.addOption(verbose);
-	
-	return options;
+        return false;
     }
 
-    static CommandLine parseOptions(String cmdName, Options options, 
-				    String[] args) {
-	CommandLineParser parser = new BasicParser();
-	CommandLine cmdLine = null;
-	try {
-	    cmdLine = parser.parse( options, args );
-	} catch ( ParseException e ) {
-	    log.error("Parsing failed.", e);
-	    System.err.println("");
-	    System.err.println("Parsing failed.  Reason: " + e.getMessage() );
-	    System.err.println("");
-	    usage(cmdName, options);
-	}
-	
-	return cmdLine;
+    public Collection findLeftIdenticalTables( ) {
+        if ( !leftIdenticalTables.isEmpty( ) ) return leftIdenticalTables;
+
+        for ( Iterator i = leftDatabase.getTables( true ).iterator( ); i
+                .hasNext( ); ) {
+            GusTable table = (GusTable) i.next( );
+
+            Schema rightSchema = rightDatabase.getSchema( table.getSchema( )
+                    .getName( ) );
+            if ( rightSchema == null ) continue;
+            Table rightTable = rightSchema.getTable( table.getName( ) );
+            if ( rightTable == null ) continue;
+
+            if ( !table.equals( rightTable ) ) continue;
+            if ( !table.columnsEqual( rightTable ) ) continue;
+            if ( !table.subclassesEqual( rightTable ) ) continue;
+            if ( !table.constraintsEqual( rightTable ) ) continue;
+            if ( !table.indexesEqual( rightTable ) ) continue;
+            leftIdenticalTables.add( table );
+        }
+        return leftIdenticalTables;
     }
 
-    static void usage(String cmdName, Options options) {
-	String newline = System.getProperty( "line.separator" );
+    public HashMap findLeftRenamedTables( ) {
+        if ( !leftRenamedTables.isEmpty( ) ) return leftRenamedTables;
+        for ( Iterator i = leftDatabase.getTables( true ).iterator( ); i
+                .hasNext( ); ) {
+            GusTable table = (GusTable) i.next( );
+            if ( findLeftIdenticalTables( ).contains( table ) ) continue;
+            if ( rightDatabase.getSchema( table.getSchema( ).getName( ) ) != null
+                    && rightDatabase.getSchema( table.getSchema( ).getName( ) )
+                            .getTable( table.getName( ) ) != null ) continue;
+            Collection renamedTables = findRenameMatches( table, 0 );
+            if ( !renamedTables.isEmpty( ) ) {
+                leftRenamedTables.put( table, renamedTables );
+            }
+        }
 
-	String cmdlineSyntax = cmdName +
-	    " [-xmlA file | -dbA dsn username password ] " +
-	    " [-xmlB file | -dbB dsn username password ] " +
-	    " -verbose";
-
-	String header = newline +
-	    "Compares two database schemas, whose source may either " +
-	    "be an XML representation, or an actual database instance. " + 
-	    "Without -verbose, simply returns whether the two schemas " + 
-	    "differ." + newline + newline + "Options: " ;
-
-	String footer = "";
-
-	HelpFormatter formatter = new HelpFormatter();
-	formatter.printHelp(75, cmdlineSyntax, header, options, footer);
-	System.exit(1);
+        return leftRenamedTables;
     }
 
-	
+    public HashMap findLeftChangedTables( ) {
+        if ( !leftChangedTables.isEmpty( ) ) return leftChangedTables;
+        for ( Iterator i = leftDatabase.getTables( true ).iterator( ); i
+                .hasNext( ); ) {
+            GusTable table = (GusTable) i.next( );
+            if ( findLeftIdenticalTables( ).contains( table ) ) continue;
+            if ( findLeftRenamedTables( ).get( table ) != null ) continue;
+            if ( rightDatabase.getSchema( table.getSchema( ).getName( ) ) == null
+                    || rightDatabase.getSchema( table.getSchema( ).getName( ) )
+                            .getTable( table.getName( ) ) == null ) continue;
+            Table rightTable = rightDatabase.getSchema(
+                    table.getSchema( ).getName( ) ).getTable( table.getName( ) );
+            Collection colCompareResults = compareColumnSets( table
+                    .getColumnsExcludeSuperclass( false ), rightTable
+                    .getColumnsExcludeSuperclass( false ) );
+            if ( !colCompareResults.isEmpty( ) ) {
+                leftChangedTables.put( table, colCompareResults );
+            }
+            // TODO Constraints
+            // TODO Indexes
+        }
+        return leftChangedTables;
+    }
+
+    public Collection findLeftDroppedTables( ) {
+        if ( !leftDroppedTables.isEmpty( ) ) return leftDroppedTables;
+        for ( Iterator i = leftDatabase.getTables( true ).iterator( ); i
+                .hasNext( ); ) {
+            GusTable table = (GusTable) i.next( );
+            if ( findLeftIdenticalTables( ).contains( table ) ) continue;
+            if ( findLeftRenamedTables( ).get( table ) != null ) continue;
+            if ( findLeftChangedTables( ).get( table ) != null ) continue;
+            leftDroppedTables.add( table );
+        }
+        return leftDroppedTables;
+    }
+
+    public Collection findRightAddedTables( ) {
+        return rightAddedTables;
+
+    }
+
+    public Collection findRenameMatches( Table table, int maxModifiedColumns ) {
+        if ( potentialRenameMatches.get( table ) != null ) return (Collection) potentialRenameMatches
+                .get( table );
+
+        Collection potentialMatches = new Vector( );
+
+        for ( Iterator i = rightDatabase.getTables( true ).iterator( ); i
+                .hasNext( ); ) {
+            Table targetTable = (Table) i.next( );
+            if ( targetTable.getSchema( ).getName( ) == table.getSchema( )
+                    .getName( )
+                    && targetTable.getName( ) == table.getName( ) ) continue;
+
+            int foundModifiedColumns = targetTable.getColumnsIncludeSuperclass(
+                    false ).size( )
+                    - table.getColumnsIncludeSuperclass( false ).size( );
+            if ( foundModifiedColumns < 0 ) foundModifiedColumns = 0;
+
+            for ( Iterator k = table.getColumnsIncludeSuperclass( false )
+                    .iterator( ); k.hasNext( ); ) {
+                Column column = (Column) k.next( );
+                if ( targetTable.getColumn( column.getName( ) ) == null ) foundModifiedColumns++;
+                else if ( !column.equals( targetTable.getColumn( column
+                        .getName( ) ) ) ) foundModifiedColumns++;
+            }
+            if ( foundModifiedColumns > maxModifiedColumns ) continue;
+            potentialMatches.add( targetTable );
+        }
+        potentialRenameMatches.put( table, potentialMatches );
+        return potentialMatches;
+    }
+
+    public Collection compareColumnSets( Collection leftColumns,
+            Collection rightColumns ) {
+        Collection results = new Vector( );
+
+        Object[] leftColumnsA = leftColumns.toArray();
+        Object[] rightColumnsA = rightColumns.toArray();
+        
+        
+        int l = 0;
+        int r = 0;
+        while ( l < leftColumnsA.length && r < rightColumnsA.length ) {
+            Column leftColumn = (Column) leftColumnsA[l];
+            Column rightColumn = (Column) rightColumnsA[r];
+
+            if ( leftColumn.equals( rightColumn ) ) {
+                l++;
+                r++;
+            }
+            else {
+                // Column attributes changed
+                if ( leftColumn.getName( ).compareToIgnoreCase(
+                        rightColumn.getName( ) ) == 0 ) {
+                    results.add( leftColumn.getName( ) + " changed " );
+                    // TODO changed details
+                    l++;
+                    r++;
+                }
+                // Testing for left column being dropped:
+                else if ( rightColumn.getTable( ).getColumn(
+                        leftColumn.getName( ) ) == null ) {
+                    results.add( leftColumn.getName( ) + " removed from table" );
+                    l++;
+                }
+                // Testing for new right column:
+                else if ( leftColumn.getTable( ).getColumn(
+                        rightColumn.getName( ) ) == null ) {
+                    results.add( rightColumn.getName( ) + " added to table" );
+                    r++;
+                }
+                else {
+                    results.add( "Error:  Confused about changes for "
+                            + leftColumn.getName( ) + "(l) and "
+                            + rightColumn.getName( ) + "(r)" );
+                    l++;
+                    r++;
+                }
+            }
+
+        }
+        while ( r < rightColumnsA.length ) {
+            Column rightColumn = (Column) rightColumnsA[r];
+            results.add( rightColumn.getName( ) + " added to table" );
+            r++;
+        }
+        while ( l < leftColumnsA.length ) {
+            Column leftColumn = (Column) leftColumnsA[l];
+            results.add( leftColumn.getName( ) + " removed from table" );
+            l++;
+        }
+        return results;
+    }
+
 }
