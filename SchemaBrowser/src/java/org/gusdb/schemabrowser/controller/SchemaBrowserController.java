212159310c4fb1c7f368368182593c2fdc678162
Michael Saffitz <m@saffitz.com>
2005-08-10 14:36:57 +0000

    removed column population as an optimization
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/GusAppFramework/trunk@3270 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- SchemaBrowser/src/java/org/gusdb/schemabrowser/controller/SchemaBrowserController.java
+++ SchemaBrowser/src/java/org/gusdb/schemabrowser/controller/SchemaBrowserController.java
@@ -8,7 +8,6 @@ import java.util.Iterator;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.gusdb.dbadmin.model.GusColumn;
 import org.gusdb.dbadmin.model.GusTable;
 import org.gusdb.schemabrowser.DatabaseFactory;
 import org.gusdb.schemabrowser.dao.DocumentationDAO;
@@ -16,7 +15,7 @@ import org.springframework.web.servlet.mvc.Controller;
 
 public abstract class SchemaBrowserController implements Controller {
 
-    protected final Log        log          = LogFactory.getLog( getClass( ) );
+    protected final Log               log          = LogFactory.getLog( getClass( ) );
     protected static DatabaseFactory  dbFactory;
     protected static DocumentationDAO docDAO;
     protected static boolean          docPopulated = false;
@@ -27,12 +26,6 @@ public abstract class SchemaBrowserController implements Controller {
             GusTable table = (GusTable) i.next( );
             table.setDocumentation( getDocumentationDAO( ).getDocumentation( table.getSchema( ).getName( ),
                     table.getName( ) ) );
-     /*       for ( Iterator j = table.getColumnsExcludeSuperclass( false ).iterator( ); j.hasNext( ); ) {
-                GusColumn col = (GusColumn) j.next( );
-                col.setDocumentation( getDocumentationDAO( ).getDocumentation( table.getSchema( ).getName( ),
-                        table.getName( ), col.getName( ) ) );
-            }
-        */
         }
     }
 
