e3f93ad7c0592f31316e0bf954743e8b3ac13df3
Steve Fischer <sfischer@upenn.edu>
2005-09-13 00:26:23 +0000

    typo
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/GusAppFramework/trunk@3411 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Supported/plugin/perl/InsertSequenceFeatures.pm
+++ Supported/plugin/perl/InsertSequenceFeatures.pm
@@ -1,7 +1,6 @@
 package GUS::Supported::Plugin::InsertSequenceFeatures;
 
 # todo:
-#  - take seq taxon on cmdline
 #  - add logging info
 #  - break special cases into plugable modules
 #  - undo
@@ -327,7 +326,7 @@ sub new {
   bless($self, $class);
 
   $self->initialize({requiredDbVersion => 3.5,
-		     cvsRevision => '$Revision: 1.20 $',
+		     cvsRevision => '$Revision: $',
 		     name => ref($self),
 		     argsDeclaration => $argsDeclaration,
 		     documentation => $documentation
