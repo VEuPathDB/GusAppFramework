61b0d7622b93844238d86d0e3511ec1547cabe50
Jonathan Schug <jschug@upenn.edu>
2005-10-09 20:02:32 +0000

    initial entry
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/GusAppFramework/trunk@3832 4235af6a-31f9-0310-9ad3-ecf6348f2534

---
+++ Community/plugin/perl/Report/ListSchema.pm
@@ -0,0 +1,156 @@
+
+package GUS::Community::Plugin::Report::ListSchema;
+@ISA = qw(GUS::PluginMgr::Plugin);
+
+# ----------------------------------------------------------------------
+
+use strict;
+
+use FileHandle;
+use V;
+
+use GUS::PluginMgr::Plugin;
+
+use GUS::PluginMgr::PluginUtilities::ConstraintFunction;
+use GUS::PluginMgr::PluginUtilities;
+
+use GUS::Model::SRes::ExternalDatabase;
+
+# ======================================================================
+
+sub new {
+   my $Class = shift;
+
+   my $self = bless {}, $Class;
+
+   my $selfPodCommand = 'pod2text '. __FILE__;
+   my $selfPod        = `$selfPodCommand`;
+
+   $self->initialize
+   ({ requiredDbVersion => '3.5',
+      cvsRevision       => ' $Revision: 2892 $ ',
+      name              => ref($self),
+
+      # just expand
+      revisionNotes     => '',
+      revisionNotes     => 'initial creation and testing',
+
+      # ARGUMENTS
+      argsDeclaration   =>
+      [
+      ],
+
+      # DOCUMENTATION
+      documentation     =>
+      { purpose          => <<Purpose,
+
+  This plugin lists Core.Database, Core.TableInfo, Core.DatabaseInfo.
+
+Purpose
+        purposeBrief     => 'List SRes.ExternalDatabase',
+        tablesAffected   =>
+        [ ],
+        tablesDependedOn =>
+        [ [ 'Core.DatabaseInfo',          'reads databases from this table'  ],
+          [ 'Core.TableInfo',             'reads tables from this table'  ],
+          [ 'Core.DatabaseDocumentation', 'reads documentation for tables and rows' ]
+        ],
+        howToRestart     => 'just restart; this is a reader',
+        failureCases     => 'just run again',
+        notes            => $selfPod,
+      },
+    });
+
+   # RETURN
+   $self
+}
+
+# ======================================================================
+
+sub run {
+   my $Self       = shift;
+
+   $Self->logArgs();
+   
+   # get maximum width of databasenames
+   my ($table_w) = @{$Self->sql_get_as_array
+   ('select max(length(name)) from Core.TableInfo')
+   };
+   
+   # get maximum width of columnnames
+   my ($column_w) = @{$Self->sql_get_as_array
+   ('select max(length(attribute_name)) from Core.DatabaseDocumentation')
+   };
+   $column_w = 32 if $column_w > 32;
+
+   
+   # get all non-version databases in order by name
+   my @db_ids = 
+   @{$Self->sql_get_as_array("select database_id from Core.DatabaseInfo")};
+   
+   my @_dbs = 
+   sort { uc $a->getName() cmp uc $b->getName() }
+   grep { $_->getName() !~ /Ver$/ }
+   map  { 
+     my $_db = GUS::Model::Core::DatabaseInfo->new({database_id => $_});
+     if ($_db->retrieveFromDB()) {
+       ($_db)
+     } else {
+       ()
+     }
+   } @db_ids;
+   
+   # process each database
+   foreach my $_db (@_dbs) {
+   
+      print '=' x 120, "\n";
+      print join("\t", 'DB', $_db->getName(),
+                 $_db->getId(),
+                 #$_db->getVersion(),
+                 $_db->getDescription()
+                ), "\n";
+                  
+      my @_tables = 
+      sort { uc $a->getName() cmp uc $b->getName() }
+      grep { $_->getTableType() ne 'version' }
+      $_db->getChildren('Core::TableInfo',1);
+      
+      foreach my $_table (@_tables) {
+      
+         print '-' x 120, "\n";
+      
+         my $table = sprintf("%-${table_w}.${table_w}s", $_table->getName());
+         my $kind  = $_table->getIsView() ? 'VIEW' : 'TBL';
+
+         print join("\t", $kind, $_db->getName(), $table,
+                    $_table->getPrimaryKeyColumn(),
+                   ), "\n";
+                   
+         my @_docs = 
+         sort { $a->getAttributeName() cmp $b->getAttributeName() }
+         $_table->getChildren('Core::DatabaseDocumentation', 1);
+          
+         foreach my $_doc (@_docs) {
+         
+            my $att = sprintf("%-${column_w}.${column_w}s",
+                              $_doc->getAttributeName() || 'TABLE'
+                             );
+               
+         
+            my $doc = $_doc->getHtmlDocumentation();
+            chomp $doc;
+
+            print join("\t", 'ROW',
+                       $_db->getName(), $table,
+                       $att,
+                       $doc
+                      ), "\n";
+          }
+      }
+   }
+
+}
+
+# ----------------------------------------------------------------------
+
+1;
---
+++ Community/plugin/perl/Report/ListXdbs.pm
@@ -0,0 +1,122 @@
+
+package GUS::Community::Plugin::Report::ListXdbs;
+@ISA = qw(GUS::PluginMgr::Plugin);
+
+# ----------------------------------------------------------------------
+
+use strict;
+
+use FileHandle;
+use V;
+
+use GUS::PluginMgr::Plugin;
+
+use GUS::PluginMgr::PluginUtilities::ConstraintFunction;
+use GUS::PluginMgr::PluginUtilities;
+
+use GUS::Model::SRes::ExternalDatabase;
+
+# ======================================================================
+
+sub new {
+   my $Class = shift;
+
+   my $self = bless {}, $Class;
+
+   my $selfPodCommand = 'pod2text '. __FILE__;
+   my $selfPod        = `$selfPodCommand`;
+
+   $self->initialize
+   ({ requiredDbVersion => '3.5',
+      cvsRevision       => ' $Revision: 2892 $ ',
+      name              => ref($self),
+
+      # just expand
+      revisionNotes     => '',
+      revisionNotes     => 'initial creation and testing',
+
+      # ARGUMENTS
+      argsDeclaration   =>
+      [ booleanArg ({ name       => 'pad',
+                      descr      => 'show usage information about the plugins',
+                      reqd       => 0,
+                      isList     => 0,
+                      constraintFunc => sub { CfIsAnything(@_) },
+                    }),
+      ],
+
+      # DOCUMENTATION
+      documentation     =>
+      { purpose          => <<Purpose,
+
+  This plugin lists SRes.ExternalDatabases and their releases.
+
+Purpose
+        purposeBrief     => 'List SRes.ExternalDatabase',
+        tablesAffected   =>
+        [ ],
+        tablesDependedOn =>
+        [
+         [ 'SRes.ExternalDatabase',        'duh!' ],
+         [ 'SRes.ExternalDatabaseRelease', 'duh!' ],
+        ],
+        howToRestart     => 'just restart; this is a reader',
+        failureCases     => 'just run again',
+        notes            => $selfPod,
+      },
+    });
+
+   # RETURN
+   $self
+}
+
+# ======================================================================
+
+sub run {
+   my $Self       = shift;
+
+	 #$Self->logArgs();
+
+   my $name_fmt    = '%s';
+   my $version_fmt = '%s';
+
+   if ($Self->getArg('pad')) {
+      my $sql = 'select max(length(name)) from SRes.ExternalDatabase';
+      my ($w) = @{$Self->sql_get_as_array($sql)};
+      $name_fmt = "%-${w}.${w}s";
+
+      my $sql = 'select max(length(version)) from SRes.ExternalDatabaseRelease';
+      my ($w) = @{$Self->sql_get_as_array($sql)};
+      $version_fmt = "%-${w}.${w}s";
+   }
+
+   my $sql = 'select external_database_id,name from SRes.ExternalDatabase order by name';
+   my @xdb_ids = @{$Self->sql_get_as_array($sql)};
+
+   for (my $i = 0; $i < @xdb_ids; $i += 2 ) {
+      my $xdb_id = $xdb_ids[$i];
+
+      my $_xdb = GUS::Model::SRes::ExternalDatabase->new
+      ({ external_database_id => $xdb_id });
+      $_xdb->retrieveFromDB();
+
+      foreach my $_xdbr (sort { $a->getId() <=> $b->getId() }
+                         $_xdb->getChildren('SRes::ExternalDatabaseRelease',1)
+                        ) {
+
+         $Self->log('XDBR',
+                    $_xdb->getId()           || 'xdb.id?',
+                    sprintf($name_fmt,    $_xdb->getName() || 'Xdb.name?'),
+
+                    $_xdbr->getId()          || 'Xdbr.id?',
+                    sprintf($version_fmt, $_xdbr->getVersion()     || 'Xdbr.ver?'),
+                    sprintf('%-24.24s',   $_xdbr->getReleaseDate() || 'Xdbr.rd?'),
+                    $_xdbr->getDescription() || 'Xdbr.descr?'
+                   );
+      }
+   }
+}
+
+# ----------------------------------------------------------------------
+
+1;
