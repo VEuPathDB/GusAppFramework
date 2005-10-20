8c69a5f9877a939022725718d8f96dee93039fa2
Deborah Pinney <pinney@upenn.edu>
2005-10-20 16:32:49 +0000

    turning keyword supstition
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/GusAppFramework/trunk@3900 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Supported/plugin/perl/CalculateTranslatedAASequences.pm
+++ Supported/plugin/perl/CalculateTranslatedAASequences.pm
@@ -82,7 +82,7 @@ sub new {
   bless $self, $class;
 
   $self->initialize({ requiredDbVersion => 3.5,
-		      cvsRevision =>  '$Revision: 3863 $',
+		      cvsRevision =>  '$Revision$',
 		      name => ref($self),
 		      argsDeclaration   => $argsDeclaration,
 		      documentation     => $documentation
