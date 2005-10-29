6c8028a7ad5ecffb099fc2eb3fd2616d0ea4915e
Steve Fischer <sfischer@upenn.edu>
2005-10-29 02:28:59 +0000

    improve comments
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/GusAppFramework/trunk@3977 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Community/plugin/perl/Undo.pm
+++ Community/plugin/perl/Undo.pm
@@ -12,13 +12,44 @@ use strict;
 use GUS::PluginMgr::Plugin;
 
 my $purpose = <<PURPOSE;
-Undo one or more runs of GUS::Supported::Plugin::InsertSequenceFeatures.  Uses the algorithm_invocation_id to find the data to remove (find this the log of the plugin run).
+The Undo plugin is very simple:
 
-If the ISF plugin mapping file uses special case handlers, their undoAll() method is called as part of the undo process.   They are called in the order the handlers are declared in the mapping file.
+  - it takes a plugin name and a list of algorithm_invocation_ids as arguments
+
+  - when it runs, it calls a method on the target plugin:   \$targetPlugin->undo(\$algInvIds, \$dbh)
+
+  - if the target plugin has not defined such a method, Undo will fail.
+
+  - if it has, then that method is responsible for performing the undo.  
+
+  - it also provides a convenient method GUS::Community::Plugin::Undo::deleteFromTable(\$table, \$algInvIds, \$dbh).  This method removes from the specified table all rows with any of the specified algortithm_invocation_ids.
+
+
+The strategy of an plugin's undo method should be:
+
+  1. call deleteFromTable on every table that it writes rows into (either directly or indirectly)
+
+  2. do so in the proper order, so that children are deleted before parents (otherwise you'll get constraint violations)
+
+  3. some tables will need special processing.  For example, deleting from tables that have links to themselves (eg NAFeature) must either proceed from child to parent (which is tricky), or, if the links are nullable, must have those links deleted first, before the rows are deleted.
+
+This is something of a use-at-your-own-risk plugin.  
+
+The risks are:
+
+  1. you will mistakenly remove an incorrect algorithm_invocation_id, thereby losing valuable data
+
+  2. the undo method in the target plugin could be written incorrectly such that it forgets to delete from some tables.  There is some protection against this because most tables that a plugin writes to are child tables.  It is not possible to forget those because that would cause a constraint violation.  It is only a problem if the Undo forgets to delete from tables that have no parents (or whose parents are also forgotten).
+
+The advantages are:
+
+  1. a correctly written undo() method is much more trustworthy than deleting by hand
+
+  2. undoing becomes doable
 PURPOSE
 
   my $purposeBrief = <<PURPOSEBRIEF;
-Undo one or more runs of GUS::Supported::Plugin::InsertSequenceFeatures.
+Undo one or more runs of a specified plugin.
 PURPOSEBRIEF
 
   my $notes = <<NOTES;
@@ -82,14 +113,16 @@ sub new {
 sub run{
   my ($self) = @_;
 
-  my $pluginName = $self->getArg('pluginName');
+  my $pluginName = $self->getArg('plugin');
   $self->{'algInvocationIds'} = $self->getArg('algInvocationId');
   $self->{'dbh'} = $self->getQueryHandle();
   $self->{'dbh'}->{AutoCommit}=0;
 
   my $plugin = eval "require $pluginName; $pluginName->new()";
 
-  $plugin->undoAll($self->{algInvocationIds}, $self->{dbh});
+  $self->error("'$pluginName' is not a valid plugin name") unless $plugin;
+
+  $plugin->undo($self->{algInvocationIds}, $self->{dbh});
 
   if ($self->getArg('commit')) {
     print STDERR "Committing\n";
