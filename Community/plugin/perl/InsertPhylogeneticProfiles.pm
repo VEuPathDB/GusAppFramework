1a1beb6739823c7c1c426c1a6a73a84807a8b091
John Brestelli <jbrestell@gmail.com>
2005-11-28 19:25:49 +0000

    degug
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/GusAppFramework/trunk@4232 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Community/plugin/perl/InsertPhylogeneticProfiles.pm
+++ Community/plugin/perl/InsertPhylogeneticProfiles.pm
@@ -8,9 +8,12 @@ package GUS::Community::Plugin::InsertPhylogeneticProfiles;
 
 use strict;
 use GUS::PluginMgr::Plugin;
+use GUS::Model::DoTS::PhylogeneticProfileSet;
 use GUS::Model::DoTS::PhylogeneticProfile;
 use GUS::Model::DoTS::PhylogeneticProfileMember;
+
 use FileHandle;
+use IO::gzip;
 
   my $purposeBrief = "Load phylogenetic profiles from a file.";
 
@@ -48,12 +51,7 @@ my $documentation = { purpose          => $purpose,
 		      notes            => $notes
 		    };
 
-my $argsDeclaration =
-  [integerArg({name           => 'phylogeneticProfileSetId',
-	       descr          => '',
-	       reqd           => 1,
-	       constraintFunc => undef,
-	       isList         => 0, }),
+my $argsDeclaration = [
 
  stringArg({ descr => 'Desciption of the PhylogeneticProfileSet',
 	     name  => 'ProfileSetDescription',
@@ -62,21 +60,30 @@ my $argsDeclaration =
 	     constraintFunc => undef,
 	   }),
 
+ fileArg({name           => 'headerFile',
+	  descr          => 'File containing Genbank Taxon Names',
+	  reqd           => 1,
+	  mustExist      => 1,
+	  format         => 'Each Line Contains a Taxon Names',
+	  constraintFunc => undef,
+	  isList         => 0, 
+	 }),
+
    fileArg({name            => 'datafile',
 	    descr           => '',
 	    reqd            => 1,
 	    mustExist       => 1,
 	    format          =>
-'This file contains a header record listing taxonIDs, separated by greater-than symbols, followed by a record for each gene.  The gene records are as in this example:
->pfalciparum|101717199  1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 0.085 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 0.053 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 0.034 0.087 0.047 1.000 0.043 1.000 0.007 1.000 1.000
+'This file (can be .gz) contains a record for each gene.  The gene records are as in this example:
+>pfalciparum|Pfa3D7|pfal_chr1|PFA0135w|Annotation|Sanger        1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 0.062 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 0.002 1.000 1.000 
 where:
 pfalciparum is a taxon name
-101717199 is the na_feature_id of the gene
-and the remaining values are presence-or-absence calls for homologs of this genein each of the taxa whose IDs are given in the first record
+PFA0135w is the Dots.GeneFeature.source_id of the gene
+and the remaining values after the tab are presence-or-absence calls for homologs of this genein each of the taxa whose IDs are given in the first record
 ',
 	    constraintFunc  => undef,
 	    isList          => 0, }),
-  ];
+                      ];
 
 sub new {
   my ($class) = @_;
@@ -98,23 +105,26 @@ sub run {
   $self->logAlgInvocationId;
   $self->logCommit;
 
-  my $datafile = $self->getArg('datafile');
-
   my $phylogeneticProfileSet = GUS::Model::DoTS::PhylogeneticProfileSet->
     new({DESCRIPTION => $self->getArg('ProfileSetDescription'),
-         PRED_ALG_INVOCATION_ID => $self->getAlgInvocationId()->getId(),
+         PRED_ALG_INVOCATION_ID => $self->getAlgInvocation()->getId(),
          });
 
   $phylogeneticProfileSet->submit();
 
-  my $fh = FileHandle->new('<'.$datafile);
-  if (! $fh) {
-      die ("Can't open datafile $datafile");
+  my $datafile = $self->getArg('datafile');
+
+  if ($datafile =~ /(.*)\.gz$/){
+    system("gunzip $datafile");
+    $fileName = $1;
+  }
+  else {
+    $fileName = $datafile;
   }
 
-  my $taxonLine = <$fh>;
-  chomp($taxonLine);
-  my @taxonIdList = split(/>/, $taxonLine);
+  open(FILE, "< $filename") || die "Can't open datafile $filename";
+
+  my $taxonIdList = $self->_getTaxonIds();
 
   my ($cCalls, $hCalls, $eCalls, $aa_sequence_id);
   while (<$fh>) {
@@ -123,14 +133,12 @@ sub run {
       my ($geneDoc, $valueString) = split(/\t/, $_);
       my $sourceId = (split(/\|/, $geneDoc))[1];
 
-      print STDERR "SourceId = $sourceId\n";
-
       my $sql = <<SQL;
       SELECT max(na_feature_id)
 	  FROM DoTS.geneFeature
 	  WHERE source_id='$sourceId'
 SQL
-	  my $sth = $self->prepareAndExecute($sql);
+      my $sth = $self->prepareAndExecute($sql);
       my ($naFeatureId) = $sth->fetchrow_array();
 
       if (!$naFeatureId) {
@@ -143,25 +151,53 @@ SQL
 	  na_feature_id => $naFeatureId,
       });
 
-      $profile->submit();
-
       my @values = split(/ /, $valueString);
       for (my $i=0; $i <= $#values; $i++) {
 
 	  my $profileMember = GUS::Model::DoTS::PhylogeneticProfileMember->new({
-	      phylogenetic_profile_id => $profile->getPhylogeneticProfileId(),
-	      taxon_id => $taxonIdList[$i],
+	      taxon_id => $taxonIdList->[$i],
 	      minus_log_e_value => $values[$i]*1000,
-	  });
+	  })->setParent($profile);
 
-	  $profileMember->submit();
-
+	  $profile->submit();
       }
 
       $self->undefPointerCache();
   }
   $fh->close();
 
+}
+
+sub _getTaxonIds {
+  my ($self) = @_;
+
+  my $fn = $self->getArgs()->{headerFile};
+
+  open(HEADER, "< $fn") || die "Cannot open $fn for reading: $!";
+
+  my @rv;
+  my $count = 0;
+
+  while(my $name = <HEADER>) {
+    chomp($name);
+    $count++;
+
+    my $sql = "SELECT taxon_id FROM SRes.TaxonName WHERE name = '$name'";
+    my $sh = $self->getQueryHandle->prepare($sql);
+    $sh->execute();
+
+    while(my ($taxonId) = $sh->fetchrow_array()) {
+      push(@rv, $taxonId);
+    }
+  }
+
+  close(HEADER);
+
+  if($count =! scalar @rv) {
+    die "The number of taxonNames provided in the header file does not match the number of taxons found in the db\n";
+  }
+
+  return(\@rv);
 }
 
 1;
