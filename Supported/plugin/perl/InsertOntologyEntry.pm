e782ff80a0c1013f0cdd7cbfaa591cdf2e7d88eb
Patricia L. Whetzel <whetzel@_.com>
2005-12-12 21:43:11 +0000

    modify plugin so that MGED Ontology and Sequence Ontology name and version are passed from the command-line and use getExtDbRelIds method to find the appropriate values
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/GusAppFramework/trunk@4313 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Supported/plugin/perl/InsertOntologyEntry.pm
+++ Supported/plugin/perl/InsertOntologyEntry.pm
@@ -18,18 +18,31 @@ use GUS::PluginMgr::Plugin;
 
 $| = 1;
 
-
 my $argsDeclaration =
 [
 
- fileArg({name => 'inputFile',
-	  descr => 'name of the file with OntologyEntry entries',
-	  constraintFunc => undef,
-	  reqd => 1,
-	  isList => 0,
-	  mustExist => 1,
-	  format => 'Text'
+fileArg({name => 'inputFile',
+	 descr => 'name of the file with OntologyEntry entries',
+	 constraintFunc => undef,
+	 reqd => 1,
+	 isList => 0,
+	 mustExist => 1,
+	 format => 'Text'
         }),
+
+stringArg({name  => 'moExternalDatabaseSpec',
+		descr => 'The name|version of the external database release of the MGED Ontology to point to',
+		constraintFunc=> undef,
+		reqd  => 1,
+		isList => 0
+	       }),
+
+stringArg({name  => 'soExternalDatabaseSpec',
+		descr => 'The name|version of the external database release of the Sequence Ontology to point to',
+		constraintFunc=> undef,
+		reqd  => 1,
+		isList => 0
+	       }),
  ];
 
 
@@ -89,6 +102,9 @@ sub run {
     my $count=0;
     my $oeFile = $self->getArg('inputFile');
 
+    my $dbh = $self->getQueryHandle();
+    #my $extDbRelIds = $self->getExtDbRelIds($dbh); #can this be called elsewhere?
+
     open(OE_FILE, $oeFile) || die "Couldn't open file '$oeFile'\n";
     
     #declare our three major data structures
@@ -108,7 +124,7 @@ sub run {
 	#split attributes, @data is one row from file
 	my @data = split (/\t/, $_);
 	
-	if (scalar(@data) != 12) { 
+	if (scalar(@data) != 11) { 
 	    print join (", ", @data);
 	    die "Input file '$oeFile' does not have 12 tab delimited files.Please check file and run plugin again\n";
 	} 
@@ -138,6 +154,21 @@ sub run {
     return "Inserted $count terms into OntologyEntry";
 }
 
+sub getExtDbRelIds {
+  my ($self, $dbh) = @_;
+  my $extDbRelIds;
+
+  $self->log("Getting the external database release ids.");
+  
+  $extDbRelIds->{'mo'} = $self->getExtDbRlsId($self->getArg('moExternalDatabaseSpec'));
+  $self->logDebug("MGED Ontology ExtDbRlsId: $extDbRelIds->{'mo'}");
+
+  $extDbRelIds->{'so'} = $self->getExtDbRlsId($self->getArg('soExternalDatabaseSpec'));
+  $self->logDebug("Sequence Ontology ExtDbRlsId: $extDbRelIds->{'so'}");
+
+   return $extDbRelIds;
+}
+
 
 sub submitOntologyEntryTree {
   my ($self, $id) = @_;
@@ -145,7 +176,7 @@ sub submitOntologyEntryTree {
   my $ontologyEntry = $self->makeOntologyEntry($id);
   
   #check that entry does not already exist
-  my $dbh = $self->getQueryHandle();
+  my $dbh = $self->getQueryHandle(); #NOTE: this is now specified in the run method so may not be needed here
   my $category = $ontologyEntry->getCategory();
   my $value = $ontologyEntry->getValue();
   
@@ -174,22 +205,41 @@ sub makeOntologyEntry {
 
    my ($self, $id) = @_;
    my $pName =              $self->{rowsById}->{$id}->[1];
-   my $extDbUri =           $self->{rowsById}->{$id}->[2];
-   my $extDbRelVersion =    $self->{rowsById}->{$id}->[3];
-   my $srcId =              $self->{rowsById}->{$id}->[4];
-   my $tName =              $self->{rowsById}->{$id}->[5];
-   my $rName =              $self->{rowsById}->{$id}->[6];
-   my $val =                $self->{rowsById}->{$id}->[7];
-   my $def =                $self->{rowsById}->{$id}->[8];
-   my $name =               $self->{rowsById}->{$id}->[9];
-   my $uri =                $self->{rowsById}->{$id}->[10];
-   my $cat =                $self->{rowsById}->{$id}->[11];
+   my $extDbSource =           $self->{rowsById}->{$id}->[2]; #NOTE: row will be marked with source of term from DumpStudyOntologyEntry plugin
+   #my $extDbRelVersion =    $self->{rowsById}->{$id}->[3]; #NOTE: this will be a value from the cla and then queried out from db instance
+   my $srcId =              $self->{rowsById}->{$id}->[3];
+   my $tName =              $self->{rowsById}->{$id}->[4];
+   my $rName =              $self->{rowsById}->{$id}->[5];
+   my $val =                $self->{rowsById}->{$id}->[6];
+   my $def =                $self->{rowsById}->{$id}->[7];
+   my $name =               $self->{rowsById}->{$id}->[8];
+   my $uri =                $self->{rowsById}->{$id}->[9];
+   my $cat =                $self->{rowsById}->{$id}->[10];
 
    my $dbh = $self->getQueryHandle();
-
+   my $extDbRelIds = $self->getExtDbRelIds($dbh);
+   
    # NOTE: The value of transformation_protocol_series must be loaded 
    # into the OntologyEntry table since it is needed by the RAD Applications 
    
+   my $extDbRelId;
+   if ($extDbSource eq "MO_term")  {
+       print STDERR "Term is from MO\n";
+       #if ($val ne "transformation_protocol_series")  { 
+	   $extDbRelId =  $extDbRelIds->{'mo'};
+       #}
+   }
+   elsif ($extDbSource eq "SO_term")  {
+       print STDERR "Term is from SO";
+       $extDbRelId = $extDbRelIds->{'so'};
+   }
+   else {
+       print STDERR "Term is required for RAD code.  No external_database_release_id exists\n";
+   }
+
+
+=pod
+   # the above code for$extDbRelId replaces the commented out code
    my $extDbRelId;
    if ($extDbUri eq "http://mged.sourceforge.net/ontologies/MGEDOntology.daml")  {
        print STDERR "Term is from MO\n";
@@ -206,7 +256,8 @@ sub makeOntologyEntry {
        $sth->execute();
        $extDbRelId = $sth->fetchrow_array();
    }
-   
+=cut   
+
    #Get table_id based on table name
    my $sth = $dbh->prepare("select table_id from core.tableinfo where name like '$tName'");
    $sth->execute();
@@ -235,7 +286,7 @@ sub makeOntologyEntry {
        'uri' =>  $uri   
       });
   
-   #print STDERR "TID:$tableId\tRID:$rowId\tPID:$parentId\tVAL:$val\tNAME:$name\tDEF:$def\tCAT:$cat\tEDBR:$extDbRelId\tSRCID:$srcId\tURI:$uri\n";
+   print STDERR "TID:$tableId\tRID:$rowId\tPID:$parentId\tVAL:$val\tNAME:$name\tDEF:$def\tCAT:$cat\tEDBR:$extDbRelId\tSRCID:$srcId\tURI:$uri\n";
 
    return $oeTerm;
 }
