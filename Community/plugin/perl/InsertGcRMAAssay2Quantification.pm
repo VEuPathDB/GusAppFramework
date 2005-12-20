82ff48abf3ae9a091e1ff64cb46eb0960a818ca0
Regina Gorski <rgorski@med.mail.upenn.edu>
2005-12-20 19:50:14 +0000

    update svn revision #
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/GusAppFramework/trunk@4353 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Community/plugin/perl/InsertGcRMAAssay2Quantification.pm
+++ Community/plugin/perl/InsertGcRMAAssay2Quantification.pm
@@ -265,7 +265,7 @@ sub new {
 
   $self->initialize({
     requiredDbVersion => 3.5,
-    cvsRevision       => '$Revision: 4350 $',
+    cvsRevision       => '$Revision: 4352 $',
     name              => ref($self),
     revisionNotes     => '',
     argsDeclaration   => $argsDeclaration,
