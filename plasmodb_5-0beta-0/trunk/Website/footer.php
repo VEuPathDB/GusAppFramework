4875e749ddc28abaa0fd6767f0f9fde4d0dc7425
yongchan <yongchan@_.com>
2006-02-09 16:32:25 +0000

    added by mistake
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/GusAppFramework/internalBranches@4514 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- plasmodb_5-0beta-0/trunk/Website/footer.php
+++
@@ -1,5 +0,0 @@
-
-</div></div>
-<br /><br />
-<div id="foot"><small><a href="mailto:webmaster@gusdb.org">webmaster@gusdb.org</a> | Design by <a href="http://smallpark.org">SmallPark</a></small></div>
-</body></html>
