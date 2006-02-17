5a126285ce3696de47e473f2284ec14aa1460cb5
Aaron Mackey <amackey@_.com>
2006-02-17 01:01:58 +0000

    solve exotic taxon name problem
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/GusAppFramework/trunk@4573 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Supported/plugin/perl/InsertSequenceFeatures.pm
+++ Supported/plugin/perl/InsertSequenceFeatures.pm
@@ -872,14 +872,18 @@ sub getTaxonId {
   my $species = $bioperlSeq->species();
   my $sciName;
   if ($species) {
-    $sciName = $species->genus() . " " . $species->species();
+    # for exotic taxa, common name is more likely to match in NCBI
+    # Taxonomy than whatever BioPerl guesses the genus/species names
+    # to be:
+    $sciName = $species->common_name();
   } else {
     $sciName = $self->getArg('defaultOrganism');
     if (!$sciName) {
       my $acc = $bioperlSeq->accession_number();
       $self->userError("Sequence '$acc' does not have organism information, and, you have not supplied a --defaultOrganism argument on the command line");
     }
-    $sciName =~ /\w+ \w+/ || $self->userError("Command line argument '--defaultOrganism $sciName' is not in 'genus species' format");
+    # this is an invalid assumption, e.g. 'Blastocrithidia sp. ex Triatoma garciabesi':
+    # $sciName =~ /\w+ \w+/ || $self->userError("Command line argument '--defaultOrganism $sciName' is not in 'genus species' format");
   }
 
   return $self->getIdFromCache('taxonNameCache',
