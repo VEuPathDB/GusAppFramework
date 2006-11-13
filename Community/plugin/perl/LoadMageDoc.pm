54dffc955fde634cdbdf369ff84b1173ab96f8c1
John Brestelli <jbrestell@gmail.com>
2006-11-13 23:46:33 +0000

    Add test for mage loader
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/GusAppFramework/trunk@5126 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Community/plugin/perl/LoadMageDoc.pm
+++ Community/plugin/perl/LoadMageDoc.pm
@@ -3,41 +3,73 @@ package GUS::Community::Plugin::LoadMageDoc;
 
 use strict;
 use Error qw(:try);
-use Config::General;
 
 use GUS::PluginMgr::Plugin;
 
 use RAD::MR_T::MageImport::ServiceFactory;
+use RAD::MR_T::MageImport::Service::Translator::VoToGusTranslator;
 
 my $documentation = {};
 
-my $argsDeclaration 
-  =[
-    fileArg({name           => 'magefile',
-	     reqd           => 1,
-	     mustExist      => 1,
-	    }),
-    fileArg({name           => 'configfile',
-	     reqd           => 1,
-	     mustExist      => 1,
-	    })
-   ];
+sub getArgumentsDeclaration { 
+ return [ fileArg({name           => 'configfile',
+                   format         => '',
+                   descr          => '', 
+                   reqd           => 1,
+                   mustExist      => 1,
+                   constraintFunc => undef,
+                   isList => '',
+                  })
+        ];
+}
 
+
+sub getDocumentation {
+  # TODO:  Fill all this in
+  my $purposeBrief = "";
+
+  my $purpose = "";
+
+  my $tablesAffected;
+
+  my $tablesDependedOn;
+
+  my $howToRestart = "";
+
+  my $failureCases = "";
+
+  my $notes = "";
+
+  my $documentation = {purpose=>$purpose, purposeBrief=>$purposeBrief, tablesAffected=>$tablesAffected, tablesDependedOn=>$tablesDependedOn, howToRestart=>$howToRestart, failureCases=>$failureCases,notes=>$notes};
+
+  return $documentation;
+}
+
+# ----------------------------------------------------------------------
+# create and initalize new plugin instance.
+# ----------------------------------------------------------------------
 
 sub new {
   my ($class) = @_;
-  my $self    = {};
+  my $self = {na_sequences => []
+             };
   bless($self,$class);
 
+  my $documentation = &getDocumentation();
+  my $argumentDeclaration = &getArgumentsDeclaration();
+
+
   $self->initialize({requiredDbVersion => 3.5,
-                     cvsRevision       => '$Revision: $',
-                     name              => ref($self),
-                     argsDeclaration   => $argsDeclaration,
-		     documentation     => $documentation
-                    });
+		     cvsRevision => '$Revision:  1 $',
+                     name => ref($self),
+                     revisionNotes => '',
+                     argsDeclaration => $argumentDeclaration,
+                     documentation => $documentation});
 
   return $self;
 }
+
+
 
 =head plugin classes
 
@@ -46,9 +78,6 @@ sub new {
 =item run method
 
 this is the main method
-
-to-dos:  need to refactor out some of the methods into ServiceFactory, ObjectMapper class
-         definetly need configuration checks
 
 config file example:
          service=reader,validator,processor
@@ -60,330 +89,40 @@ the idea here is to have serviceFactory to read this config file and automatical
 
 =cut
 
-sub run {
+sub _run {
   my ($self) = @_;
 
   my %config = ParseConfig((-ConfigFile => $self->getArgs('configfile'), -AutoTrue => 1));
 
   my $serviceFactory = RAD::MR_T::MageImport::ServiceFactory->new(\%config);
 
-  #  my $docType = $config{docType};
-  #  my $reader = $self->createReader($docType);
-
   my $reader =  $serviceFactory->getServiceByName('reader');
   my $docRoot = $reader->parse($self->getArgs('magefile'));
 
   if($config{modules}){
     my @pModules = split(/,/, $config{modules});
-    #  my $processor = $self->createProcessor(\@pModules);
+
     my $processor = $serviceFactory->getServiceByName('processor');;
     $processor->process($docRoot);
   }
 
   if($config{rules}){
     my @rules = split(/,/, $config{rules});
-    #  my $validator = $self->createValidator(\@rules);
+
     my $validator = $serviceFactory->getServiceByName('validator');;
     $validator->check($docRoot);
   }
 
-  my $study = $self->map2GUSObjTree($docRoot);
+  my $translator = $serviceFactory->getServiceByName('translator');
+  my $study = $translator->mapAll($docRoot);
 
   $study->submit;
-}
 
-# this method can move to a new class ObjectMapper
-sub map2GUSObjTree {
- my ($self, $docRoot) = @_;
-
- my $radStudy;
- my @radAffiliations;
- my @radContacts;
- my @radExternalDbs;
- my @radAssays;
- my @radProtocols;
- my @radBioMaterials;
- my @radTreatments;
-
-
- $self->createRadAffiliations($docRoot->affiliationVOs(), \@radAffiliations);
-
- $self->createRadContacts($docRoot->personVOs(), \@radContacts, \@radAffiliations);
-
- $self->createRadExternalDatabases($docRoot->externalDatabaseVOs(), \@radExternalDbs);
-
- $radStudy = $self->getRadStudy($docRoot->getStudyVO());
-
- if(my $name = $docRoot->getStudyVO()->getContact()->getName()){
-   $self->setRadStudyContact($name, $radStudy, \@radContacts);
- }
-
- $self->createRadStudyDesignAndFactors($docRoot->getStudyVO()->getDesigns, $radStudy);
-
- $self->createRadProtocols($docRoot->getProtcolVOs, \@radProtocols);
-
- $self->createRadAssays($docRoot->getAssayVOs, \@radAssays);
-
- $self->createRadBioMaterials($docRoot->getBioMaterialVOs, \@radBioMaterials, \@radProtocols);
-
- $self->createRadTreatments($docRoot->getTreatmentVOs, \@radTreatments, \@radProtocols, \@radBioMaterials);
-
- $self->linkRadStudyAssay($radStudy, \@radAssays);
- $self->linkRadStudyBioMaterial($radStudy, \@radBioMaterials);
-
- return $radStudy;
+  # Sql Tester
 }
 
 
 
-sub createRadAffiliations{
-  my ($self, $mageAffs, $radAffiliations) = @_;
-  foreach my $mageAff (@ $mageAffs){
-    my %h;
-    $h{name} = $mageAff->getName if $mageAff->getName;
-    my $radAff = GUS::Model::SRes::Contact->new(%h);
-    push @$radAffiliations, $radAff;
-  }
-}
 
-
-sub createRadContacts{
- my ($self, $magePersons, $radContacts, $radAffiliations) = @_;
- foreach my $mageContact (@ $magePersons ){
-   my %h;
-   $h{name} = $mageContact->getName if $mageContact->getName;
-   $h{last} = $mageContact->getLast if $mageContact->getLast;
-   $h{first} = $mageContact->getFirst if $mageContact->getFirst;
-
-   my $radContact = GUS::Model::SRes::Contact->new(%h);
-
-   if(my $name = $mageContact->getAffiliation->getName){
-     $radContact->setParent($self->searchObjArrayByObjName($radAffiliations, $name)) ;
-   }
-
-   push @$radContacts, $radContact;
- }
-
-}
-
-# we have to assume edb  is already in RAD
-sub createRadExternalDatabases{
-  my ($self, $mageExtDbs, $radExtDbs) = @_;
-  foreach my $mageExtDb (@ $mageExtDbs){
-    my %h;
-    $h{name} = $mageExtDb->getName if $mageExtDb->getName;
-    my $radDb = GUS::Model::SRes::ExternalDatabase->new(%h);
-    my $radDbRel;
-
-    if ($mageExtDb->getVersion){
-      my %hh;
-      $hh{version} = $mageExtDb->getName; 
-      $radDbRel = GUS::Model::SRes::ExternalDatabaseRelease->new(%hh);
-    }
-    else{
-      $radDbRel = GUS::Model::SRes::ExternalDatabaseRelease->new();
-    }
-
-    $radDbRel->setParent($radDb);
-    RAD::MR_T::MageImport::MageImportExtDbNotFoundError->new(ref($self)."->createRadExternalDatabases")->throw() unless $radDbRel->retrieveFromDB;
-    push @$radExtDbs, $radDbRel;
-  }
-}
-
-sub getRadStudy{
-  my ($self, $mageStudy) = @_;
-  my %h;
-  $h{name} = $mageStudy->getName() if $mageStudy->getName();
-  $h{description} = $mageStudy->getDescription() if $mageStudy->getDescription();
-
-  my $radStudy = GUS::Model::Study::Study->new(%h);
-
-  return $radStudy;
-}
-
-sub setRadStudyContact{
-  my ($self, $name, $radStudy, $radContacts) = @_;
-  $radStudy->setParent($self->searchObjArrayByObjName($radContacts, $name) );
-}
-
-sub createRadStudyDesignAndFactors{
- my ($self, $mageSDs, $radStudy) = @_;
- foreach my $mageSD (@ $mageSDs){
-   my %h;
-   $h{name} = $mageSD->getName() if $mageSD->getName();
-   my $radSD = GUS::Model::Study::StudyDesign->new(%h);
-   $radSD->setParent($radStudy);
-
-   my $radSDType = GUS::Model::Study::StudyDesignType->new();
-   $radSDType->setParent($radSD);
-   $radSDType->setParent($self->createRadOE($mageSD->getType, "studyDesign"));
-
-   $self->createRadStudyFactors($mageSD->getFactors, $radSD);
- }
-}
-
-sub createRadStudyFactors{
-  my ($self, $mageSFs, $radSD) = @_;
-   foreach my $mageSF (@ $mageSFs){
-     my %h;
-     $h{name} = $mageSF->getName() if $mageSF->getName();
-     my $radSF = GUS::Model::Study::StudyFactor->new(%h);
-     $radSF->setParent($radSD);
-     $radSF->setParent($self->createRadOE($mageSF->getType, "studyFactor"));
-   }
-}
-
-
-sub createRadProtocols{
- my ($self, $mageProtocols, $radProtocols) = @_;
- foreach my $mageProtocol(@ $mageProtocols){
-   my %h;
-   $h{name} = $mageProtocol->getName if $mageProtocol->getName;
-   $h{uri} = $mageProtocol->getUri if $mageProtocol->getUri;
-   $h{protocolDescription} = $mageProtocol->getProtocolDescription if $mageProtocol->getProtocolDescription;
-   $h{softwareDescription} = $mageProtocol->getSoftwareDescription if $mageProtocol->getSoftwareDescription;
-   $h{hardwareDescription} = $mageProtocol->getHardwareDescription if $mageProtocol->getHardwareDescription;
-
-   my $radProtocol = GUS::Model::RAD::Protocol->new(%h);
-   $radProtocol->setParent($self->createRadOE($mageProtocol->getProtocolType, "protocol")) if $mageProtocol->getProtocolType;
-
-   $self->createRadProtocolParams($mageProtocol->getParams, $radProtocol);
- }
-}
-
-
-sub createRadProtocolParams{
-  my ($self, $mageProtocolParams, $radProtocol) = @_;
-  foreach my $magePP(@ $mageProtocolParams){
-    my %h;
-    $h{name} = $magePP->getName if $magePP->getName;
-    $h{value} = $magePP->getValue if $magePP->getValue;
-    my $radProtocolParam = GUS::Model::RAD::ProtocolParam->new(%h);
-    $radProtocolParam->setParent($radProtocol);
-
-    $radProtocolParam->setParent($self->createRadOE($magePP->getUnitType, "unit")) if $magePP->getUnitType;
-    $radProtocolParam->setParent($self->createRadOE($magePP->getDataType, "data")) if $magePP->getDataType;
-  }
-}
-
-sub createRadAssays{
- my ($self, $mageAssays, $radAssays) = @_;
- foreach my $mageAssay(@ $mageAssays){
-   my %h;
-   $h{name} = $mageAssay->getName if $mageAssay->getName;
-   my $radAssay = GUS::Model::RAD::Assay->new(%h);
-
-   push @$radAssays, $radAssay;
-
-   $self->createRADAcquisitions($mageAssay->getAcquisitions, $radAssay);
- }
-}
-
-
-sub createRadAcquisitions{
-  my ($self, $mageAcquisitions, $radAssay) = @_;
-  foreach my $mageAcquisition(@ $mageAcquisitions){
-    my %h;
-    $h{name} = $mageAcquisition->getName if $mageAcquisition->getName;
-    my $radAcquisition = GUS::Model::RAD::Acquisition->new(%h);
-    $radAcquisition->setParent($radAssay);
-    $self->createRADQuantifications($mageAcquisition->getQuantifications, $radAcquisition);
-  }
-}
-
-sub createRadQuantifications{
-  my ($self, $mageQuantifications, $radAcquisition) = @_;
-  foreach my $mageQuantification(@ $mageQuantifications){
-    my %h;
-    $h{name} = $mageQuantification->getName if $mageQuantification->getName;
-    my $radQuantification = GUS::Model::RAD::Quantification->new(%h);
-    $radQuantification->setParent($radAcquisition);
-  }
-}
-
-sub createRadBioMaterials{
-  my ($self, $mageBioMaterials, $radBioMaterials, $radProtocols) = @_;
-  foreach my $mageBM(@ $mageBioMaterials){
-    my %h;
-    $h{name} = $mageBM->getName if $mageBM->getName;
-    $h{description} = $mageBM->getDescription if $mageBM->getDescription;
-    $h{subclass_view} = $mageBM->getSubclassView if $mageBM->getSubclassView;
-
-    my $radBM = GUS::Model::Study::BioMaterial->new(%h);
-
-    $radBM->setParent($self->createRadOE($mageBM->getBioMaterialType, "biomaterial")) if $mageBM->getBioMaterialType;
-
-    $radBM->setParent($self->searchObjArrayByObjName($radProtocols), $mageBM->getLabelMethod->getName) if $mageBM->getLabelMethod->getName;
-  }
-}
-
-sub createRadTreatments{
-  my ($self, $mageTreatments, $radTreatments, $radProtocols, $radBioMaterials) = @_;
-  foreach my $mageTrt(@ $mageTreatments){
-    my %h;
-    $h{name} = $mageTrt->getName if $mageTrt->getName;
-    $h{order_num} = $mageTrt->getOrderNum if $mageTrt->getOrderNum;
- 
-    my $radTrt = GUS::Model::RAD::Treatment->new(%h);
-
-    $radTrt->setParent($self->createRadOE($mageTrt->getTreatmentType, "treatment")) if $mageTrt->getTreatmentType;
-
-    $radTrt->setParent($self->searchObjArrayByObjName($radProtocols, $mageTrt->getProtocol->getName)) if $mageTrt->getProtocol->getName;
-
-    $radTrt->setParent($self->searchObjArrayByObjName($radBioMaterials, $mageTrt->getOutputBM->getName)) if $mageTrt->getOutputBM->getName;
-
-    my %hh;
-    $hh{value} = $mageTrt->getInputBMM->getValue;
-    my $radBMM = GUS::Model::RAD::BioMaterialMeasurement->new(%hh);
-    $radBMM->setParent($radTrt) if $mageTrt->getInputBMM;
-    $radBMM->setParent($self->searchObjArrayByObjName($radBioMaterials, $mageTrt->getInputBMM->getBioMaterial->getName)) if $mageTrt->getInputBMM->getBioMaterial->getName;
-    $radBMM->setParent($self->createRadOE($mageTrt->getInputBMM->getUnitType, "unit")) if $mageTrt->getInputBMM->getUnitType;
-  }
-}
-
-# we have to assume OE is already in RAD
-sub createRadOE{
-  my ($self, $mageOE, $type) = @_;
-
-  my @modes    = qw( studyDesign studyFactor treatment protocol unit data biomaterial);
-  my $modes_rx = '^('. join('|',@modes). ')$';
-
-  if ($type =~ /$modes_rx/) {
-    my $radOE = GUS::Model::Study::OntologyEntry->new({value=>$mageOE->getValue, category=>$mageOE->getCategory});
-    RAD::MR_T::MageImport::MageImportOENotFoundError->new(
-							  ref($self)."->createRadOE: OntologyEntry value: ".$mageOE->getValue."category: ".$mageOE->getCategory
-							 )->throw() unless $radOE->retrieveFromDB;
-    return $radOE;
-  }
-  else {
-
-  }
-}
-
-sub linkRadStudyAssay{
-  my ($self, $radStudy, $radAssays) = @_;
-  foreach my $radAssay (@$radAssays){
-    my $radStudyAssay = GUS::Model::RAD::StudyAssay->new();
-    $radStudyAssay->setParent($radAssay);
-    $radStudyAssay->setParent($radStudy);
-  }
-}
-
-sub linkRadStudyBioMaterial{
- my ($self, $radStudy, $radBioMaterials) = @_;
-  foreach my $radBioMaterial (@$radBioMaterials){
-    my $radStudyBioMaterial = GUS::Model::RAD::StudyBioMaterial->new();
-    $radStudyBioMaterial->setParent($radBioMaterial);
-    $radStudyBioMaterial->setParent($radStudy);
-  }
-}
-
-#Util methods
-sub searchObjArrayByObjName{
- my ($self, $objArray, $name) = @_;
- foreach my $obj(@$objArray){
-   return $obj if $obj->getName eq $name;
- }
-}
 
 1;
