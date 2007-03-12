3fc761045e7293c037863a8ce18d701037ee999b
Deborah Pinney <pinney@upenn.edu>
2007-03-12 19:54:36 +0000

    update DbVersion number
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/GusAppFramework/trunk@5413 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Community/plugin/perl/InsertDbRefAndDbRefNASequence.pm
+++ Community/plugin/perl/InsertDbRefAndDbRefNASequence.pm
@@ -52,7 +52,7 @@ sub new {
      }
     ];
 
-  $self->initialize({requiredDbVersion => {},
+  $self->initialize({requiredDbVersion => 3.5,
 		  cvsRevision => '$Revision$', # cvs fills this in!
 		  cvsTag => '$Name$', # cvs fills this in!
 		  name => ref($self),
