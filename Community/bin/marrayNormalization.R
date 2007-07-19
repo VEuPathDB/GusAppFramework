741a02a9e5067b4768ac41b6fd542239ffd6db51
Elisabetta Manduchi <manduchi@upenn.edu>
2007-07-19 19:07:48 +0000

    changes cmd line code and streamlined code
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/GusAppFramework/trunk@5649 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Community/bin/marrayNormalization.R
+++ Community/bin/marrayNormalization.R
@@ -3,33 +3,9 @@
 # GUS::Community::Plugin::CreateAndInsertRmarrayResults and it performs 
 # loess normalization as implemented in the marray package from 
 # www.bioconductor.org.
-# R CMD BATCH --slave --no-save --output_file=/tmp/loess.Routput --input_file=/tmp/loess.Rinput --smoothing_param=0.3 --maxiter=3 [--print_tip_loess] --ngr=4 --ngc=4 --nsr=22 --nsc=22 
+# echo 'inputFile="loess.Rinput";outputFile="loess.Routput";smoothingParam=0.3;ngr=4;ngc=4;nsr=22;nsc=22;printTip=T' | cat - $thisScript | R --slave --no-save";  
 
 library(marray)
-
-# parsing command line arguments
-argin <- commandArgs()
-
-inputFile <- gsub("--input_file=","",grep("--input_file=",argin,value=T))
-cat ("Input file: ", inputFile, "\n")
-
-outputFile <- gsub("--output_file=","",grep("--output_file=",argin,value=T))
-cat ("Output file: ", outputFile, "\n")
-
-smoothingParam <- gsub("--smoothing_param=","",grep("--smoothing_param=",argin,value=T)) 
-smoothingParam <- as.numeric(smoothingParam)
-
-printTip <- grep("--print_tip_loess",argin)
-
-ngr <- gsub("--ngr=","",grep("--ngr=",argin,value=T))
-ngc <- gsub("--ngc=","",grep("--ngc=",argin,value=T))
-nsr <- gsub("--nsr=","",grep("--nsr=",argin,value=T))
-nsc <- gsub("--nsc=","",grep("--nsc=",argin,value=T))
-ngr <- as.numeric(ngr)
-ngc <- as.numeric(ngc)
-nsr <- as.numeric(nsr)
-nsc <- as.numeric(nsc)
-
 
 data <- read.table(inputFile, header=T)
 # header: element_id Rf Rb Gf Gb
@@ -48,7 +24,7 @@ mat <- as.matrix(data[,2:5])
 row.names(mat) <- data[,1]
 data<-new("marrayRaw",maRf=as.matrix(mat[,1]), maRb=as.matrix(mat[,2]), maGf=as.matrix(mat[,3]), maGb=as.matrix(mat[,4]), maLayout=layout)
 
-if (any(printTip)) {
+if (printTip) {
   norm.data <- maNorm(data, norm="printTipLoess", span=smoothingParam)
 } else {
   norm.data <- maNorm(data, norm="loess", span=smoothingParam)
