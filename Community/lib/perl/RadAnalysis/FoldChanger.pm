03c0610c92bba9fde82c49d3f47a4a98f7dd352b
John Brestelli <jbrestell@gmail.com>
2007-12-05 17:09:32 +0000

    distinguish between paire and unpaired for missing values
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/GusAppFramework/trunk@5842 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Community/lib/perl/RadAnalysis/FoldChanger.pm
+++ Community/lib/perl/RadAnalysis/FoldChanger.pm
@@ -3,6 +3,8 @@ package GUS::Community::RadAnalysis::FoldChanger;
 use GUS::Model::RAD::Protocol;
 use GUS::Model::RAD::ProtocolParam;
 
+use GUS::Community::RadAnalysis::RadAnalysisError;
+
 use CBIL::Util::V;
 
 use GUS::Community::RadAnalysis::Utils qw(getOntologyEntriesHashFromParentValue);
@@ -13,7 +15,7 @@ sub new {
   my ($class, $dir) = @_;
 
   unless(-d $dir) {
-    die "Directory [$dir] does not exist: $!";
+    GUS::Community::RadAnalysis::DirectoryAccessError->new("Directory [$dir] does not exist")->throw();
   }
 
   my $dataTypeOeHash = &getOntologyEntriesHashFromParentValue('DataType');
@@ -78,7 +80,7 @@ sub getProtocol{
 
     my $typeOe = $self->getDataTransformationOeHash()->{across_bioassay_data_set_function};
     unless($typeOe) {
-      die "Did NOT retrieve Study::OntologyEntry [across_bioassay_data_set_function]";
+      GUS::Community::RadAnalysis::SqlError->new("Did NOT retrieve Study::OntologyEntry [across_bioassay_data_set_function]")->throw();
     }
 
     $protocol->setProtocolTypeId($typeOe->getId());
@@ -108,7 +110,7 @@ sub makeFileHandle {
 
   my $fn = join("_vs_", @names) . ".txt";
 
-  open(FILE, "> $fn") or die "Cannot open file [$fn] for writing: $!";
+  open(FILE, "> $fn") or GUS::Community::RadAnalysis::FileAccessError->new("Cannot open file [$fn] for writing: $!")->throw();
 
   return \*FILE;
 }
@@ -117,7 +119,7 @@ sub makeFileHandle {
 
 # This is the default... any subclass below where it doesn't apply should override
 sub writeDataFile {
-  my ($self, $input, $logicalGroups, $baseX, $isDataPaired) = @_;
+  my ($self, $input, $logicalGroups, $baseX) = @_;
 
   my $header = 'ratio';
   my $MISSING_VALUE = 'NA';
@@ -167,7 +169,7 @@ sub writeDataFile {
       $value = $averages[1] / $averages[0];
     }
     else {
-      die "Wrong Number of LogicalGroups [$lgCount]";
+      GUS::Community::RadAnalysis::RadAnalysisError->new("Wrong Number of LogicalGroups [$lgCount]")->throw();
     }
 
     print $fh "$element\t\t\t$value\n";
@@ -186,7 +188,7 @@ use base qw(GUS::Community::RadAnalysis::FoldChanger);
 sub getProtocol{
 
   my $name = 'Ratio and Fold Change calculation from M values in 2-channel direct comparisons';
-  my $description = 'The input to this protocol are normalized M values from a collection of 2-channel assays comparing condition C1 and condition C2 in a direct design fashion, so that, for each reporter and each assay, M=log2(C1)-log2(C2). For each reporter its average normalized M value Mbar across the assays is first computed. Then its ratio r is set to 2^(Mbar), i.e. 2 to the Mbar power. Its fold change FC is obtained as follows. If r=1, then FC=0; if r>1, then FC=r; if r<1 then FC=-(1/r).';
+  my $description = 'The input to this protocol are normalized M values from a collection of 2-channel assays comparing condition C1 and condition C2 in a direct design fashion, so that, for each reporter and each assay, its raw M=log2(c1)-log2(c2), where by c_i we denote the intensity of the reporter in condition C_i for that assay. For each reporter its average normalized M value Mbar across the assays is first computed, after removing assays with missing values for that reporter. Then its ratio r is set to 2^(Mbar), i.e. 2 to the Mbar power. Its fold change FC is obtained as follows. If r=1, then FC=0; if r>1, then FC=r; if r<1 then FC=-(1/r).';
 
   return shift()->SUPER::getProtocol($name, $description);
 
@@ -196,13 +198,33 @@ sub getProtocol{
 
 #================================================================================
 
-package GUS::Community::RadAnalysis::TwoChannelReferenceDesign;
+package GUS::Community::RadAnalysis::TwoChannelReferenceDesignPaired;
 use base qw(GUS::Community::RadAnalysis::FoldChanger);
 
 sub getProtocol{
 
-  my $name = 'Ratio and Fold Change calculation from M values in 2-channel reference design comparisons';
-  my $description = 'The input to this protocol are normalized M values from a collection of 2-channel assays comparing condition C1 and condition C2 in a reference design fashion. Thus, if the common reference is denoted by B, for each reporter and each assay in condition C1, we have M1=log2(C1)-log2(B). For each reporter and each assay in condition C2, we have M2=log2(C2)-log2(B). For each reporter, first its average normalized M1 value M1bar across the assays in condition C1 and its average normalized M2 value M2bar across the assays in condition C2 are computed. Then its ratio r is set to 2^(M1bar-M2bar), i.e. 2 to the (M1bar-M2bar) power. Its fold change FC is obtained as follows. If r=1, then FC=0; if r>1, then FC=r; if r<1 then FC=-(1/r).';
+  my $name = 'Ratio and Fold Change calculation from M values in 2-channel reference design paired comparisons';
+  my $description = 'The input to this protocol are normalized M values from a collection of 2-channel assays comparing condition C1 and condition C2 in a reference design paired fashion. Thus, if the common reference is denoted by B, for each reporter and each assay in condition C1, we have (raw) M1=log2(c1)-log2(b) where c1 and b denote the intensities of the reporter in C1 and B for that assay. For each reporter and each assay in condition C2, we have (raw) M2=log2(c2)-log2(b), with similar notation. For each reporter, first its average normalized M1 value M1bar across the assays in condition C1 and its average normalized M2 value M2bar across the assays in condition C2 are computed, after removing the paired assays where at least one of the values is missing for that reporter. Then its ratio r is set to 2^(M1bar-M2bar), i.e. 2 to the (M1bar-M2bar) power. Its fold change FC is obtained as follows. If r=1, then FC=0; if r>1, then FC=r; if r<1 then FC=-(1/r).';
+
+  return shift()->SUPER::getProtocol($name, $description);
+}
+
+# TODO: implement this!
+sub writeDataFile {
+  GUS::Community::RadAnalysis::RadAnalysisError->new("The subroutine [writeDataFile] has not yet been implemented for " . __PACKAGE__)->throw();
+}
+
+1;
+
+#================================================================================
+
+package GUS::Community::RadAnalysis::TwoChannelReferenceDesignUnpaired;
+use base qw(GUS::Community::RadAnalysis::FoldChanger);
+
+sub getProtocol{
+
+  my $name = 'Ratio and Fold Change calculation from M values in 2-channel reference design unpaired comparisons';
+  my $description = 'The input to this protocol are normalized M values from a collection of 2-channel assays comparing condition C1 and condition C2 in a reference design unpaired fashion. Thus, if the common reference is denoted by B, for each reporter and each assay in condition C1, we have (raw) M1=log2(c1)-log2(b) where c1 and b denote the intensities of the reporter in C1 and B for that assay. For each reporter and each assay in condition C2, we have (raw) M2=log2(c2)-log2(b), with similar notation. For each reporter, first its average normalized M1 value M1bar across the assays in condition C1 and its average normalized M2 value M2bar across the assays in condition C2 are computed, after removing assays with missing values for that reporter. Then its ratio r is set to 2^(M1bar-M2bar), i.e. 2 to the (M1bar-M2bar) power. Its fold change FC is obtained as follows. If r=1, then FC=0; if r>1, then FC=r; if r<1 then FC=-(1/r).';
 
   return shift()->SUPER::getProtocol($name, $description);
 }
@@ -217,14 +239,14 @@ use base qw(GUS::Community::RadAnalysis::FoldChanger);
 sub getProtocol{
 
   my $name = 'Ratio and Fold Change calculation from normalized intensities in 1-channel paired comparisons';
-  my $description = 'The input to this protocol are normalized (un-logged) intensities from a collection of 1-channel assays comparing condition C1 and condition C2 in a paired fashion. For each reporter and each pair of corresponding assays (one from condition C1 and the other from condition C2), the ratio of its intentities in the two assays c1/c2 is computed. For each reporter its ratio r is set to the average of its pairwise ratios over all pairs of correponding assays. Its fold change FC is obtained as follows. If r=1, then FC=0; if r>1, then FC=r; if r<1 then FC=-(1/r).';
-
-  # TODO: implement this!
-  sub writeDataFile {
-    die "The subroutine [writeDataFile] has not yet been implemented for " . __PACKAGE__;
-  }
+  my $description = 'The input to this protocol are normalized (un-logged) intensities from a collection of 1-channel assays comparing condition C1 and condition C2 in a paired fashion. For each reporter and each pair of corresponding assays (one from condition C1 and the other from condition C2) with no missing values for that reporter, the ratio of the reporters intentities in the two assays c1/c2 is computed. For each reporter its ratio r is set to the average of its pairwise ratios over all pairs of correponding assays with no missing values for that reporter. Its fold change FC is obtained as follows. If r=1, then FC=0; if r>1, then FC=r; if r<1 then FC=-(1/r).';
 
   return shift()->SUPER::getProtocol($name, $description);
+}
+
+# TODO: implement this!
+sub writeDataFile {
+  GUS::Community::RadAnalysis::RadAnalysisError->new("The subroutine [writeDataFile] has not yet been implemented for " . __PACKAGE__)->throw();
 }
 
 1;
@@ -237,7 +259,7 @@ use base qw(GUS::Community::RadAnalysis::FoldChanger);
 sub getProtocol{
 
   my $name = 'Ratio and Fold Change calculation from normalized intensities in 1-channel unpaired comparisons';
-  my $description = 'The input to this protocol are normalized (un-logged) intensities from a collection of 1-channel assays comparing condition C1 and condition C2 in an unpaired fashion. For each reporter, its average normalized intensity in condition C1 and its average normalized intensity in condition C2 are computed. Then its ratio r is set to the ratio of these average normalized intensities. Its fold change FC is obtained as follows. If r=1, then FC=0; if r>1, then FC=r; if r<1 then FC=-(1/r).';
+  my $description = 'The input to this protocol are normalized (un-logged) intensities from a collection of 1-channel assays comparing condition C1 and condition C2 in an unpaired fashion. For each reporter, its average normalized intensity c1 in condition C1 and its average normalized intensity c2 in condition C2 are computed, after removing assays with missing values for that reporter. Then its ratio r is set to the ratio of these average normalized intensities: c1/c2. Its fold change FC is obtained as follows. If r=1, then FC=0; if r>1, then FC=r; if r<1 then FC=-(1/r).';
 
   return shift()->SUPER::getProtocol($name, $description);
 }
@@ -246,13 +268,33 @@ sub getProtocol{
 
 #================================================================================
 
-package GUS::Community::RadAnalysis::OneChannelLogNormalized;
+package GUS::Community::RadAnalysis::OneChannelLogNormalizedPaired;
 use base qw(GUS::Community::RadAnalysis::FoldChanger);
 
 sub getProtocol{
 
-  my $name = 'Ratio and Fold Change calculation from normalized log intensities in 1-channel comparisons';
-  my $description = 'The input to this protocol are normalized log2 intensities from a collection of 1-channel assays comparing condition C1 and condition C2. For each reporter its average log2 intensities CiBar in each condition are computed. Then its ratio r is set to 2^(C1bar-C2bar), i.e. 2 to the (C1bar-C2bar) power. Its fold change FC is obtained as follows. If r=1, then FC=0; if r>1, then FC=r; if r<1 then FC=-(1/r).';
+  my $name = 'Ratio and Fold Change calculation from normalized log intensities in 1-channel paired comparisons';
+  my $description = 'The input to this protocol are normalized log2 intensities from a collection of 1-channel assays comparing condition C1 and condition C2 in a paired fashion. For each reporter its average log2 intensities CiBar in each condition are computed, after removing the paired assays where at least one of the values is missing for that reporter. Then its ratio r is set to 2^(C1bar-C2bar), i.e. 2 to the (C1bar-C2bar) power. Its fold change FC is obtained as follows. If r=1, then FC=0; if r>1, then FC=r; if r<1 then FC=-(1/r).';
+
+  return shift()->SUPER::getProtocol($name, $description);
+}
+
+# TODO: implement this!
+sub writeDataFile {
+  GUS::Community::RadAnalysis::RadAnalysisError->new("The subroutine [writeDataFile] has not yet been implemented for " . __PACKAGE__)->throw();
+}
+
+1;
+
+#================================================================================
+
+package GUS::Community::RadAnalysis::OneChannelLogNormalizedUnpaired;
+use base qw(GUS::Community::RadAnalysis::FoldChanger);
+
+sub getProtocol{
+
+  my $name = 'Ratio and Fold Change calculation from normalized log intensities in 1-channel unpaired comparisons';
+  my $description = 'The input to this protocol are normalized log2 intensities from a collection of 1-channel assays comparing condition C1 and condition C2 in an unpaired fashion. For each reporter its average log2 intensities CiBar in each condition are computed, after removing assays with missing values for that reporter. Then its ratio r is set to 2^(C1bar-C2bar), i.e. 2 to the (C1bar-C2bar) power. Its fold change FC is obtained as follows. If r=1, then FC=0; if r>1, then FC=r; if r<1 then FC=-(1/r).';
 
   return shift()->SUPER::getProtocol($name, $description);
 }
@@ -268,7 +310,7 @@ use base qw(GUS::Community::RadAnalysis::FoldChanger);
 sub getProtocol{
 
   my $name = 'Ratio and Fold Change calculation from ratios in 2-channel reference design unpaired comparisons';
-  my $description = 'The input to this protocol are normalized (un-logged) ratios from a collection of 2-channel assays comparing condition C1 and condition C2 in a reference design unpaired fashion. Thus, if the common reference is denoted by B, for each reporter and each assay in condition C1, we have R1=c1/b where c1 and b denote the intensities of the reporter in C1 and B. For each reporter and each assay in condition C2, we have R2=c2/b, with similar notation. For each reporter, first its average normalized R1 value R1bar across the assays in condition C1 and its average normalized R2 value R2bar across the assays in condition C2 are computed. Then its ratio r is set to R1bar/R2bar. Its fold change FC is obtained as follows. If r=1, then FC=0; if r>1, then FC=r; if r<1 then FC=-(1/r).';
+  my $description = 'The input to this protocol are normalized (un-logged) ratios from a collection of 2-channel assays comparing condition C1 and condition C2 in a reference design unpaired fashion. Thus, if the common reference is denoted by B, for each reporter and each assay in condition C1, we have R1=c1/b where c1 and b denote the intensities of the reporter in C1 and B for that assay. For each reporter and each assay in condition C2, we have R2=c2/b, with similar notation. For each reporter, first its average normalized R1 value R1bar across the assays in condition C1 and its average normalized R2 value R2bar across the assays in condition C2 are computed, after removing assays with missing values for that reporter. Then its ratio r is set to R1bar/R2bar. Its fold change FC is obtained as follows. If r=1, then FC=0; if r>1, then FC=r; if r<1 then FC=-(1/r).';
 
   return shift()->SUPER::getProtocol($name, $description);
 }
