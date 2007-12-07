662617fc3eec3674acfc7b388119d4bf5b35f6df
John Brestelli <jbrestell@gmail.com>
2007-12-07 20:38:19 +0000

    refactor the writeDataFile and instatnce vars in the setters of subclasses
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/GusAppFramework/trunk@5845 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Community/lib/perl/RadAnalysis/FoldChanger.pm
+++ Community/lib/perl/RadAnalysis/FoldChanger.pm
@@ -1,5 +1,7 @@
 package GUS::Community::RadAnalysis::FoldChanger;
 
+use strict;
+
 use GUS::Model::RAD::Protocol;
 use GUS::Model::RAD::ProtocolParam;
 
@@ -12,25 +14,63 @@ use GUS::Community::RadAnalysis::Utils qw(getOntologyEntriesHashFromParentValue)
 #--------------------------------------------------------------------------------
 
 sub new {
-  my ($class, $dir) = @_;
-
-  unless(-d $dir) {
-    GUS::Community::RadAnalysis::DirectoryAccessError->new("Directory [$dir] does not exist")->throw();
-  }
+  my ($class, $dir, $baseX, $isDataPaired) = @_;
 
   my $dataTypeOeHash = &getOntologyEntriesHashFromParentValue('DataType');
   my $dataTransformationOeHash = &getOntologyEntriesHashFromParentValue('DataTransformationProtocolType');
 
-  bless {data_type_oe_hash => $dataTypeOeHash,
-         data_transformation_oe_hash => $dataTransformationOeHash,
-         output_directory => $dir}, $class;
+  my $self = bless {data_type_oe_hash => $dataTypeOeHash,
+                    data_transformation_oe_hash => $dataTransformationOeHash}, $class;
+
+  $self->setOutputDirectory($dir);
+  $self->setBaseX($baseX);
+  $self->setIsDataPaired($isDataPaired);
+
+  return $self;
 }
 
 #--------------------------------------------------------------------------------
 
 sub getDataTypeOeHash {$_[0]->{data_type_oe_hash}}
 sub getDataTransformationOeHash {$_[0]->{data_transformation_oe_hash}}
+
 sub getOutputDirectory {$_[0]->{output_directory}}
+sub setOutputDirectory {
+  my ($self, $dir) = @_;
+
+  unless(-d $dir) {
+    GUS::Community::RadAnalysis::DirectoryAccessError->new("Directory [$dir] does not exist")->throw();
+  }
+
+  $self->{output_directory} = $dir;
+}
+
+#--------------------------------------------------------------------------------
+# expected is determined by the subclass
+sub getBaseX {$_[0]->{baseX}}
+sub setBaseX {
+  my ($self, $baseX, $expected) = @_;
+
+
+  if(($expected && !$baseX) || (!$expected && $baseX)) {
+    GUS::Community::RadAnalysis::RadAnalysisError->new("baseX [$baseX] is wrong for " .  ref($self))->throw();
+  }
+
+  $self->{baseX} = $baseX;
+}
+
+#--------------------------------------------------------------------------------
+# expected is determined by the subclass
+sub getIsDataPaired {$_[0]->{isDataPaired}}
+sub setIsDataPaired {
+  my ($self, $isDataPaired, $expected) = @_;
+
+  if(($expected && !$isDataPaired) || (!$expected && $isDataPaired)) {
+    GUS::Community::RadAnalysis::RadAnalysisError->new("isDataPaired [$isDataPaired] is wrong for " . ref($self))->throw();
+  }
+
+  $self->{isDataPaired} = $isDataPaired;
+}
 
 #--------------------------------------------------------------------------------
 
@@ -72,6 +112,10 @@ sub setupProtocolParams {
 
 sub getProtocol{
   my ($self, $name, $description) = @_;
+
+  unless($name && $description) {
+    GUS::Community::RadAnalysis::RadAnalysisError->new("Please implement the getProtocol method for " . ref($self))->throw();
+  }
 
   my $protocol = GUS::Model::RAD::Protocol->new({name => $name});
 
@@ -117,64 +161,143 @@ sub makeFileHandle {
 
 #--------------------------------------------------------------------------------
 
-# This is the default... any subclass below where it doesn't apply should override
-sub writeDataFile {
-  my ($self, $input, $logicalGroups, $baseX) = @_;
+sub printHeader {
+  my ($self, $baseX, $fh) = @_;
 
   my $header = 'ratio';
-  my $MISSING_VALUE = 'NA';
-
-  my $lgCount = scalar(@$logicalGroups);
 
   if($baseX) {
     $header = "log" . $baseX . $header;
   }
+  print $fh "row_id\t$header\n";
+}
+
+#--------------------------------------------------------------------------------
+
+sub calculatePairedAverages {
+  my ($self, $dataHash, $logicalGroups, $na) = @_;
+
+  unless(scalar @$logicalGroups == 2) {
+    GUS::Community::RadAnalysis::RadAnalysisError->new("Expected 2 logical groups as input to paired analysis")->throw();
+  }
+
+  my @averages;
+
+  my @logicalGroupNames = map {$_->getName()} @$logicalGroups;
+
+  my @data0 = @{$dataHash->{$logicalGroupNames[0]}};
+  my @data1 = @{$dataHash->{$logicalGroupNames[1]}};
+
+  unless(scalar @data0 == scalar @data1) {
+    print STDERR join(',', @data0) . "\n";
+    print STDERR join(',', @data1) . "\n";
+    GUS::Community::RadAnalysis::RadAnalysisError->new("Unequal Number of data points for Paired Analysis")->throw();
+  }
+
+  my (@output0, @output1);
+
+  for(my $i = 0; $i < scalar @data0; $i++) {
+    next if($data0[$i] eq $na || $data1[$i] eq $na);
+
+    push(@output0, $data0[$i]);
+    push(@output1, $data1[$i]);
+  }
+
+  # Don't average if they are all NA's
+  if(scalar(@output0) == 0) {
+    push(@averages, $na, $na);
+  }
+  else {
+    $averages[0] = CBIL::Util::V::average(@output0);
+    $averages[1] = CBIL::Util::V::average(@output1);
+  }
+
+  return \@averages;
+}
+
+#--------------------------------------------------------------------------------
+
+sub calculateUnpairedAverages {
+  my ($self, $dataHash, $logicalGroups, $na) = @_;
+
+  my @averages;
+
+  foreach my $lg (@$logicalGroups) {
+    my @output;
+
+    foreach(@{$dataHash->{$lg->getName}}) {
+      push(@output, $_) unless($_ eq $na);
+    }
+
+    # Don't average if they are all NA's
+    if(scalar(@output) == 0) {
+      push(@averages, $na);
+    }
+    else {
+      push(@averages, CBIL::Util::V::average(@output));
+    }
+  }
+  return \@averages;
+}
+
+#--------------------------------------------------------------------------------
+
+sub calculateRatio {
+  my ($self, $lgCount, $baseX, $averages) = @_;
+
+  my $value;
+
+  if($lgCount == 1) {
+    $value = $averages->[0];
+  }
+  elsif($lgCount == 2 && $baseX) {
+    $value = $averages->[1] - $averages->[0];
+  }
+  elsif($lgCount == 2 && !$baseX) {
+    $value = $averages->[1] / $averages->[0];
+  }
+  else {
+    GUS::Community::RadAnalysis::RadAnalysisError->new("Wrong Number of LogicalGroups [$lgCount]")->throw();
+  }
+  return $value;
+}
+
+#--------------------------------------------------------------------------------
+
+# Any subclass below can override this if need be
+sub writeDataFile {
+  my ($self, $input, $logicalGroups) = @_;
+
+  my $baseX = $self->getBaseX();
+  my $isDataPaired = $self->getIsDataPaired();
+
+  my $MISSING_VALUE = 'NA';
+  my $lgCount = scalar(@$logicalGroups);
 
   my $fh = $self->makeFileHandle($logicalGroups);
-
-  print $fh "row_id\tconfidence_up\tconfidence_down\t$header\n";
+  $self->printHeader($baseX, $fh);
 
   foreach my $element (keys %$input) {
-    my @averages;
+    my $averages;
 
-    foreach my $lg (@$logicalGroups) {
-      my @output;
+    my $dataHash = $input->{$element};
 
-      foreach(@{$input->{$element}->{$lg->getName}}) {
-        push(@output, $_) unless($_ eq $MISSING_VALUE);
-      }
-
-      # Don't average if they are all NA's
-      if(scalar(@output) == 0) {
-        push(@averages, $MISSING_VALUE);
-      }
-      else {
-        push(@averages, CBIL::Util::V::average(@output));
-      }
+    if($isDataPaired) {
+      $averages = $self->calculatePairedAverages($dataHash, $logicalGroups, $MISSING_VALUE);
+    }
+    else {
+      $averages = $self->calculateUnpairedAverages($dataHash, $logicalGroups, $MISSING_VALUE);
     }
 
     # Don't print if the averages any are NA's
     my $naCount = 0;
-    map {$naCount++ if($_ eq $MISSING_VALUE)} @averages;
+    map {$naCount++ if($_ eq $MISSING_VALUE)} @$averages;
     next if($naCount > 0);
 
-    my $value;
-    if($lgCount == 1) {
-      $value = $averages[0];
-    }
-    elsif($lgCount == 2 && $baseX) {
-      $value = $averages[1] - $averages[0];
-    }
-    elsif($lgCount == 2 && !$baseX) {
-      $value = $averages[1] / $averages[0];
-    }
-    else {
-      GUS::Community::RadAnalysis::RadAnalysisError->new("Wrong Number of LogicalGroups [$lgCount]")->throw();
-    }
+    my $ratio = $self->calculateRatio($lgCount, $baseX, $averages);
 
-    print $fh "$element\t\t\t$value\n";
+    print $fh "$element\t$ratio\n";
   }
-
   close $fh;
 }
 
@@ -184,6 +307,8 @@ sub writeDataFile {
 
 package GUS::Community::RadAnalysis::TwoChannelDirectComparison;
 use base qw(GUS::Community::RadAnalysis::FoldChanger);
+
+sub setBaseX {$_[0]->SUPER::setBaseX($_[1], 1)}
 
 sub getProtocol{
 
@@ -201,6 +326,9 @@ sub getProtocol{
 package GUS::Community::RadAnalysis::TwoChannelReferenceDesignPaired;
 use base qw(GUS::Community::RadAnalysis::FoldChanger);
 
+sub setBaseX {$_[0]->SUPER::setBaseX($_[1], 1)}
+sub setIsDataPaired {$_[0]->SUPER::setIsDataPaired($_[1], 1)}
+
 sub getProtocol{
 
   my $name = 'Ratio and Fold Change calculation from M values in 2-channel reference design paired comparisons';
@@ -209,17 +337,14 @@ sub getProtocol{
   return shift()->SUPER::getProtocol($name, $description);
 }
 
-# TODO: implement this!
-sub writeDataFile {
-  GUS::Community::RadAnalysis::RadAnalysisError->new("The subroutine [writeDataFile] has not yet been implemented for " . __PACKAGE__)->throw();
-}
-
 1;
 
 #================================================================================
 
 package GUS::Community::RadAnalysis::TwoChannelReferenceDesignUnpaired;
 use base qw(GUS::Community::RadAnalysis::FoldChanger);
+
+sub setBaseX {$_[0]->SUPER::setBaseX($_[1], 1)}
 
 sub getProtocol{
 
@@ -236,17 +361,14 @@ sub getProtocol{
 package GUS::Community::RadAnalysis::OneChannelPaired;
 use base qw(GUS::Community::RadAnalysis::FoldChanger);
 
+sub setIsDataPaired {$_[0]->SUPER::setIsDataPaired($_[1], 1)}
+
 sub getProtocol{
 
   my $name = 'Ratio and Fold Change calculation from normalized intensities in 1-channel paired comparisons';
   my $description = 'The input to this protocol are normalized (un-logged) intensities from a collection of 1-channel assays comparing condition C1 and condition C2 in a paired fashion. For each reporter and each pair of corresponding assays (one from condition C1 and the other from condition C2) with no missing values for that reporter, the ratio of the reporters intentities in the two assays c1/c2 is computed. For each reporter its ratio r is set to the average of its pairwise ratios over all pairs of correponding assays with no missing values for that reporter. Its fold change FC is obtained as follows. If r=1, then FC=0; if r>1, then FC=r; if r<1 then FC=-(1/r).';
 
   return shift()->SUPER::getProtocol($name, $description);
-}
-
-# TODO: implement this!
-sub writeDataFile {
-  GUS::Community::RadAnalysis::RadAnalysisError->new("The subroutine [writeDataFile] has not yet been implemented for " . __PACKAGE__)->throw();
 }
 
 1;
@@ -271,17 +393,15 @@ sub getProtocol{
 package GUS::Community::RadAnalysis::OneChannelLogNormalizedPaired;
 use base qw(GUS::Community::RadAnalysis::FoldChanger);
 
+sub setBaseX {$_[0]->SUPER::setBaseX($_[1], 1)}
+sub setIsDataPaired {$_[0]->SUPER::setIsDataPaired($_[1], 1)}
+
 sub getProtocol{
 
   my $name = 'Ratio and Fold Change calculation from normalized log intensities in 1-channel paired comparisons';
   my $description = 'The input to this protocol are normalized log2 intensities from a collection of 1-channel assays comparing condition C1 and condition C2 in a paired fashion. For each reporter its average log2 intensities CiBar in each condition are computed, after removing the paired assays where at least one of the values is missing for that reporter. Then its ratio r is set to 2^(C1bar-C2bar), i.e. 2 to the (C1bar-C2bar) power. Its fold change FC is obtained as follows. If r=1, then FC=0; if r>1, then FC=r; if r<1 then FC=-(1/r).';
 
   return shift()->SUPER::getProtocol($name, $description);
-}
-
-# TODO: implement this!
-sub writeDataFile {
-  GUS::Community::RadAnalysis::RadAnalysisError->new("The subroutine [writeDataFile] has not yet been implemented for " . __PACKAGE__)->throw();
 }
 
 1;
@@ -291,6 +411,8 @@ sub writeDataFile {
 package GUS::Community::RadAnalysis::OneChannelLogNormalizedUnpaired;
 use base qw(GUS::Community::RadAnalysis::FoldChanger);
 
+sub setBaseX {$_[0]->SUPER::setBaseX($_[1], 1)}
+
 sub getProtocol{
 
   my $name = 'Ratio and Fold Change calculation from normalized log intensities in 1-channel unpaired comparisons';
@@ -298,7 +420,6 @@ sub getProtocol{
 
   return shift()->SUPER::getProtocol($name, $description);
 }
-
 
 1;
 
