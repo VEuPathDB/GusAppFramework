3bc004ad81fbafead851b8e2d9ca9b02e1dc257a
Steve Fischer <sfischer@upenn.edu>
2008-07-24 00:32:21 +0000

    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/GusAppFramework/trunk@6099 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Pipeline/lib/perl/Workflow/WorkflowStep.pm
+++ Pipeline/lib/perl/Workflow/WorkflowStep.pm
@@ -21,11 +21,6 @@ sub getConfig {
     return $self->getStepConfig($propName);
 }
 
-sub getName {
-    my ($self) = @_;
-    return $self->{name};
-}
-
 sub runInWrapper {
     my ($self, $workflowId, $stepName) = @_;
 
@@ -37,7 +32,7 @@ SET
   state = '$RUNNING',
   state_handled = 1,
   process_id = $process_id,
-  start_time = $start_time
+  start_time = SYSDATE
 )
 WHERE name = $stepName
 AND workflow_id = $workflowId
@@ -59,7 +54,7 @@ UPDATE apidb.WorkflowStep
 SET (
   state = $state
   process_id = NULL
-  end_time = $end_time
+  end_time = SYSDATE
   state_handled = 0
 )
 WHERE name = $self->{name} 
@@ -69,4 +64,34 @@ AND state = '$RUNNING'
     $self->runSql($sql);
 }
 
+
+sub _runPlugin {
+    my ($self, $signal, $plugin, $args, $msg, $doitProperty) = @_;
+
+    return if $self->startStep($msg, $signal, $doitProperty);
+    
+    my $err = "$self->{pipelineDir}/logs/$signal.err";
+    my $out = "$self->{pipelineDir}/logs/$signal.out";
+
+    my $comment = $args;
+    $comment =~ s/"/\\"/g;
+
+    if ($self->{gusConfigFile}) {
+      $args .= " --gusconfigfile $self->{gusConfigFile}";
+    }
+
+    my $cmd = "ga $plugin $args --comment \"$comment\"  >> $out 2>> $err";
+
+    $self->runCmd("mkdir -p $self->{pipelineDir}/plugins/$signal");
+    chdir "$self->{pipelineDir}/plugins/$signal";
+
+    $self->runCmd($cmd);
+    if ($self->{testNextPlugin} eq "true"){
+	print STDERR "Tested next plugin.  Check $self->{pipelineDir}/logs/$signal" . ".err and $signal" . ".out for results\n\n";
+	$self->_cleanup(1);
+    }
+    else{
+	$self->endStep($signal);
+    }
+}
 
