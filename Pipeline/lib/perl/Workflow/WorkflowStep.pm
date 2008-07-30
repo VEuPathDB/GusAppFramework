57f57a97797117efd7d75e2f95c33b46971a34c0
Steve Fischer <sfischer@upenn.edu>
2008-07-30 12:29:58 +0000

    ongoing development
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/GusAppFramework/trunk@6128 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Pipeline/lib/perl/Workflow/WorkflowStep.pm
+++ Pipeline/lib/perl/Workflow/WorkflowStep.pm
@@ -19,8 +19,7 @@ my $END = 'END';
 
 # wrapper
 #  ON_DECK --> RUNNING
-#  RUNNING --> DONE
-#  RUNNING --> FAILED
+#  RUNNING --> DONE | FAILED
 #  (state_handled --> false)
 
 # Pilot UI (GUI or command line)
@@ -58,11 +57,6 @@ sub addParent {
     push(@{$self->{parents}}, $parentStep);
 }
 
-sub getChildren {
-    my ($self) = @_;
-    return $self->{children};
-}
-
 sub getParents {
     my ($self) = @_;
     return $self->{parents};
@@ -92,9 +86,25 @@ sub getState () {
     return $self->{state};
 }
 
-sub setFakeStepType {
-  my ($self, $type) = @_;
-  $self->{fakeStepType} = $type;
+# static method
+sub getBulkSnapshotSql {
+    my ($workflow_id) = @_;
+      return "
+SELECT workflow_step_id, name, host_machine, process_id, state,
+       state_handled, off_line, start_time, end_time
+FROM apidb.workflowstep
+WHERE workflow_id = $workflow_id";
+}
+
+# static method
+sub getPreparedInsertStmt {
+    my ($workflow_id) = @_;
+
+    my $sql = "
+INSERT INTO apidb.workflowstep (workflow_step_id, workflow_id, name, state, state_handled, off_line)
+VALUES (apidb.workflowstep_sq.nextval, $workflow_id, ?, ?, 1, 0)
+";
+    return $self->getDbh()->prepare($sql);
 }
 
 # write this step to the db, if not already there.
@@ -105,113 +115,64 @@ sub initializeStepTable {
     return if $self->{inStepTable};
 
     my $name = $self->getName();
-
-    # if this is the start step, create a prepared statement for all steps to use
-    # also, check if start step is in db.  if so, assume all steps are, and quit.
-    my $state = $READY;
-    if ($self->{fakeStepType} eq $START) {
-      my $workflow_id = $self->{workflow}->getId();
-      my ($count) = $self->runSqlQuery_single_array("select count(*) from apidb.workflowstep where workflow_id = $workflow_id");
-      return if $count;
-
-      my $sql = "
-INSERT INTO apidb.workflowstep (workflow_step_id, workflow_id, name, state, state_handled, off_line)
-VALUES (apidb.workflowstep_sq.nextval, $workflow_id, ?, ?, 1, 0)
-";
-      $stmt = $self->{workflow}->getDbh()->prepare($sql);
-      $state = $DONE;
-    }
+    my $state = $self->{fakeStepType} eq $START? $DONE : $READY;
     $stmt->execute($name, $state);
-    $self->{inStepTable} = 1;
-    foreach my $childStep (@{$self->getChildren()}) {
-	$childStep->initializeStepTable($stmt);
-    }
+}
+
+# static method
+sub getPreparedDependsStmt {
+    my ($workflow_id) = @_;
+
+    my $sql= "
+INSERT INTO apidb.workflowstepdependency (workflow_step_dependency_id, parent_id, child_id)
+VALUES (apidb.workflowstepdependency_sq.nextval, ?, ?)
+";
+    return $self->getDbh()->prepare($sql);
 }
 
 sub initializeDependsTable {
     my ($self,$stmt) = @_;
-    return if $self->{inDependsTable};
-    my $sql = "
-INSERT INTO apidb.workflowstepdependency (workflow_step_dependency_id, parent_id, child_id)
-VALUES (apidb.workflowstepdependency_sq.nextval, ?, ?)
-";
-    if (!$stmt) {
-	$stmt = $self->{workflow}->getDbh()->prepare($sql);
-    }
-
-    $self->{inDependsTable} = 1;
-    foreach my $childStep (@{$self->getChildren()}) {
-	$stmt->execute($self->getId(), $childStep->getId());
-	$childStep->initializeDependsTable($stmt);
+    foreach my $parentStep (@{$self->getParents()}) {
+	$stmt->execute($parentStep->getId(), $self->getId());
     }
 }
 
-# returns number of running steps (-1 if last step is done)
-# 
-sub handleChangesSinceLastPoll {
-    my ($self) = @_;
+sub setFakeStepType {
+  my ($self, $type) = @_;
+  $self->{fakeStepType} = $type;
+}
 
-    return 0 if $self->{handledSnapshot} == $self->{workflow}->{snapshotNumber};
+sub handleChangesSinceLastSnapshot {
+    my ($self, $newSnapshot) = @_;
 
-    $self->{handledSnapshot} = $self->{workflow}->{snapshotNumber};
+    my $prevState = $self->{state};
+    my $prevOffline = $self->{off_line};
 
-    # if this step has no kids, then it is the end-step
-    # if all parents done, workflow is done!
-    if (scalar(@{$self->getChildren()}) == 0 && $self->{state} eq $ON_DECK) {
-	return -1;
-    }
+    $self->getDbStateFromSnapshot($newSnapshot);
 
-    $self->getDbState();
-
-    # the wrapper or pilot UI can change the state to
-    # RUNNING, FAILED, OFFLINE, READY or DONE.
-    # here if unchanged since last time
     if ($self->{state_handled}) {
-
-      return 0 if $self->{off_line};
 
       if ($self->{state} eq $RUNNING) {
 	system("ps -p $self->{process_id} > /dev/null");
 	my $status = $? >> 8;
 	if ($status) {
 	  $self->handleMissingProcess();
-	  return 0;
 	}
-	return 1;
       }
-
-      # kids can only be active if this step is done
-      elsif ($self->{state} eq $DONE) {
-	my $count = 0;
-	foreach my $childStep (@{$self->getChildren()}) {
-	  $count += $childStep->handleChangesSinceLastPoll();
-	}
-	return $count;
-      }
-
-      elsif ($self->{state} eq $READY) {
-	$self->maybeGoToOnDeck();
-      }
-
-      # FAILED, ON_DECK
-      else {
-	return 0;
-      }
-
     } else { # this step has been changed by wrapper or pilot UI. log change.
       my $stateMsg = "";
       my $offlineMsg = "";
-      if ($self->{state} ne $self->{prevState}) {
+      if ($self->{state} ne $prevState) {
 	$stateMsg = "  $self->{state}";
       }
-      if ($self->{off_line} ne $self->{prevOffline}) {
+      if ($self->{off_line} ne $prevOffline) {
 	$offlineMsg = $self->{off_line}? "  OFFLINE" : "  ONLINE";
       }
 
       $self->log("Step '$self->{name}'$stateMsg$offlineMsg");
       $self->setHandledFlag();
-      return 0;
     }
+    return $self->{state} eq $RUNNING;
 }
 
 sub setHandledFlag {
@@ -245,64 +206,6 @@ AND state = '$RUNNING'
     $self->log("Step '$self->{name}' $FAILED (can't find wrapper process $self->{process_id})");
 }
 
-# called by pilot UI
-sub pilotKill {
-    my ($self) = @_;
-
-     $self->{lastSnapshot} = -1;
-   my ($state) = $self->getDbState();
-
-    die "Can't change to '$FAILED' from '$state'\n"
-	if ($state ne $RUNNING);
-
-    $self->{workflow}->runCmd("kill -9 $self->{process_id}");
-    $self->pilotLog("Step '$self->{name}' killed");
-}
-
-# called by pilot UI
-sub pilotSetReady {
-    my ($self) = @_;
-
-    $self->{lastSnapshot} = -1;
-    my ($state) = $self->getDbState();
-
-    die "Can't change to '$READY' from '$state'\n"
-	unless ($state eq $FAILED);
-
-    my $sql = "
-UPDATE apidb.WorkflowStep
-SET 
-  state = '$READY',
-  state_handled = 0
-WHERE workflow_step_id = $self->{workflow_step_id}
-AND state = '$FAILED'
-";
-    $self->runSql($sql);
-    $self->pilotLog("Step '$self->{name}' set to $READY");
-}
-
-# called by pilot UI
-sub pilotSetOffline {
-    my ($self, $offline) = @_;
-
-    $self->{lastSnapshot} = -1;
-    my ($state) = $self->getDbState();
-    die "Can't change OFFLINE when '$RUNNING'\n"
-	if ($state eq $RUNNING);
-    my $offline_bool = $offline eq 'offline'? 1 : 0;
-
-    my $sql = "
-UPDATE apidb.WorkflowStep
-SET
-  off_line = $offline_bool,
-  state_handled = 0
-WHERE workflow_step_id = $self->{workflow_step_id}
-AND (state != '$RUNNING')
-";
-    $self->runSql($sql);
-    $self->pilotLog("Step '$self->{name}' $offline");
-}
-
 # if this step is ready, and all parents are done, transition to ON_DECK
 sub maybeGoToOnDeck {
     my ($self) = @_;
@@ -326,40 +229,19 @@ AND state = '$READY'
 sub runOnDeckStep {
     my ($self) = @_;
 
-    my $foundOne;
     if ($self->{state} eq $ON_DECK && !$self->{off_line}) {
 	$self->forkAndRun();
-	$foundOne = 1;
+	return 1;
     } 
-    elsif ($self->{state} eq $DONE && !$self->{off_line}) {
-	foreach my $childStep (@{$self->getChildren()}) {
-	    $foundOne = $childStep->runOnDeckStep();
-	    last if $foundOne;
-	}
-    }
-    return $foundOne;
+    return 0;
 }
 
-sub getDbState {
-    my ($self) = @_;
-    if ($self->{lastSnapshot} != $self->{workflow}->{snapshotNumber}) {
-      $self->{lastSnapshot} = $self->{workflow}->{snapshotNumber};
+sub getDbStateFromSnapshot {
+    my ($self, $snapshot) = @_;
 
-      $self->{prevState} = $self->{state};
-      $self->{prevOffline} = $self->{off_line};
-
-      my $workflow_id = $self->{workflow}->getId();
-      my $sql = "
-SELECT workflow_step_id, host_machine, process_id, state,
-       state_handled, off_line, start_time, end_time
-FROM apidb.workflowstep
-WHERE name = '$self->{name}'
-AND workflow_id = $workflow_id";
-      ($self->{workflow_step_id}, $self->{host_machine}, $self->{process_id},
-       $self->{state}, $self->{state_handled}, $self->{off_line},
-       $self->{start_time}, $self->{end_time})= $self->runSqlQuery_single_array($sql);
+    foreach my $key (%$snapshot) {
+	$self->{$key} = $snapshot->{$key};
     }
-    return $self->{state};
 }
 
 sub getStepDir {
@@ -394,25 +276,9 @@ sub log {
     $self->{workflow}->log($msg);
 }
 
-sub pilotLog {
-  my ($self,$msg) = @_;
-
-  my $homeDir = $self->{workflow}->getHomeDir();
-
-  open(LOG, ">>$homeDir/logs/pilot.log")
-    || die "can't open log file '$homeDir/logs/pilot.log'";
-  print LOG localtime() . " $msg\n";
-  close (LOG);
-}
-
 sub runSql {
     my ($self,$sql) = @_;
     $self->{workflow}->runSql($sql);
-}
-
-sub runSqlQuery_single_array {
-    my ($self,$sql) = @_;
-    return $self->{workflow}->runSqlQuery_single_array($sql);
 }
 
 sub toString {
