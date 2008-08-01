3d1c3e4fda2892fe96478308186372ecccab752f
John Brestelli <jbrestell@gmail.com>
2008-08-01 01:08:25 +0000

    Revision#
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/GusAppFramework/trunk@6132 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Community/plugin/perl/InsertHomoloGene.pm
+++ Community/plugin/perl/InsertHomoloGene.pm
@@ -102,7 +102,7 @@ sub new {
   my $argumentDeclaration    = &getArgumentsDeclaration();
 
   $self->initialize({requiredDbVersion => 3.5,
-		     cvsRevision => '$Revision: 6114$',
+		     cvsRevision => '$Revision$',
 		     name => ref($self),
 		     revisionNotes => '',
 		     argsDeclaration => $argumentDeclaration,
