248174a6b6d0c443f97ad78c92eddbfef44d127d
John Brestelli <jbrestell@gmail.com>
2008-08-15 03:43:18 +0000

    add keywords
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/GusAppFramework/trunk@6173 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Community/plugin/perl/InsertGene2GoNoDbRef.pm
+++ Community/plugin/perl/InsertGene2GoNoDbRef.pm
@@ -102,7 +102,7 @@ sub new {
   bless($self,$class);
 
   $self->initialize({requiredDbVersion => 3.5,
-		     cvsRevision => '$Revision: $',
+		     cvsRevision => '$Revision$',
 		     name => ref($self),
 		     argsDeclaration   => $argsDeclaration,
 		     documentation     => $documentation
