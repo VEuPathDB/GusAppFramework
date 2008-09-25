23ffa4bde55c563a399384e210e3969e86a3618b
Steve Fischer <sfischer@upenn.edu>
2008-09-25 20:44:38 +0000

    early stages
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/GusAppFramework/trunk@6354 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Workflow/src/java/org/gusdb/workflow/WorkflowBase.java
+++ Workflow/src/java/org/gusdb/workflow/WorkflowBase.java
@@ -1,48 +1,53 @@
 package org.gusdb.workflow;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 
 public class WorkflowBase {
     public static final String READY = "READY"; // my parents are not done yet  -- default state
-    public static final String ON_DECK = 'ON_DECK';  //my parents are done, but there is no slot for me
+    public static final String ON_DECK = "ON_DECK";  //my parents are done, but there is no slot for me
     public static final String FAILED = "FAILED";
     public static final String DONE = "DONE";
     public static final String RUNNING = "RUNNING";
     public static final String WAITING_FOR_PILOT = "WAITING_FOR_PILOT";  // not used yet.
-
+    
     public static final String START = "START";
     public static final String  END = "END";
 
+    final static String nl = System.getProperty("line.separator");
+
     Connection dbConnection;
+    String homeDir;
 
     // methods shared by the perl controller and perl step wrapper.
     // any other language implementation would presumably need equivalent code
-    public void WorkflowBase(String homeDir) {
+    public WorkflowBase(String homeDir) {
 	this.homeDir = homeDir;
     }
 
-    Connection getDbConnection {
+    Connection getDbConnection() throws SQLException {
 	if (dbConnection == null) {
 	    DriverManager.registerDriver (new oracle.jdbc.driver.OracleDriver());
-
-	    Connection dbConnection = 
-		DriverManager.getConnection(getWorkflowConfig("dbConnectString"),
-					    getWorkflowConfig("dbLogin"),
-					    getWorkflowConfig("dbPassword"));
+	    dbConnection = DriverManager.getConnection(getWorkflowConfig("dbConnectString"),
+	            getWorkflowConfig("dbLogin"),
+	            getWorkflowConfig("dbPassword"));
 	}
 	return dbConnection;
     }
 
-    void runSql(String sql) {
-	Connection connection = getConnection();
-	Statement stmt = connection.createStatement();
+    void runSql(String sql) throws SQLException {
+	Statement stmt = dbConnection.createStatement();
 	stmt.executeUpdate(sql);
     }
 
-    ResultSet runSqlQuery_single_row(String sql) {
-	Statement stmt = getConnection().createStatement();
-	return stmt.executeQuery(sql);
+    ResultSet runSqlQuerySingleRow(String sql) throws SQLException {
+        Statement stmt = dbConnection.createStatement();
+        ResultSet rs = stmt.executeQuery(sql);
+        rs.next();
+        return rs;
     }
 
     String getHomeDir() {
