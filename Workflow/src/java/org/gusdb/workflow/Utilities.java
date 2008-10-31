bf9fbc576fbbbc0afa1d10acfb535d9cab931369
Steve Fischer <sfischer@upenn.edu>
2008-10-31 16:32:09 +0000

    start of dev of global steps
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/GusAppFramework/trunk@6498 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Workflow/src/java/org/gusdb/workflow/Utilities.java
+++ Workflow/src/java/org/gusdb/workflow/Utilities.java
@@ -9,6 +9,8 @@ import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
 import java.io.File;
 import java.io.IOException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class Utilities {
     
@@ -74,4 +76,23 @@ public class Utilities {
 	    dir.delete();
 	}
     }	
+    
+    public static String encrypt(String data) throws Exception,
+    NoSuchAlgorithmException {
+        // cannot encrypt null value
+        if (data == null || data.length() == 0)
+            throw new Exception("Cannot encrypt an empty/null string");
+
+        MessageDigest digest = MessageDigest.getInstance("MD5");
+        byte[] byteBuffer = digest.digest(data.toString().getBytes());
+        // convert each byte into hex format
+        StringBuffer buffer = new StringBuffer();
+        for (int i = 0; i < byteBuffer.length; i++) {
+            int code = (byteBuffer[i] & 0xFF);
+            if (code < 0x10) buffer.append('0');
+            buffer.append(Integer.toHexString(code));
+        }
+        return buffer.toString();
+    }
+    
 }
