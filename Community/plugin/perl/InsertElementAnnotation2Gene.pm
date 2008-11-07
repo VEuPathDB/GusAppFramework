68cab96187250d1466cb2ea45c3e7cf6f2439621
chiaolin <chiaolin@_.com>
2008-11-07 16:40:53 +0000

    add svn keyword to plugin
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/GusAppFramework/trunk@6515 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Community/plugin/perl/InsertElementAnnotation2Gene.pm
+++ Community/plugin/perl/InsertElementAnnotation2Gene.pm
@@ -83,7 +83,7 @@ sub new {
   my $argumentDeclaration    = &getArgumentsDeclaration();
   
   $self->initialize({requiredDbVersion => 3.5,
-		     cvsRevision => '$Revision: 6088 $',
+		     cvsRevision => '$Revision$',
 		     name => ref($self),
 		     revisionNotes => '',
 		     argsDeclaration => $argumentDeclaration,
