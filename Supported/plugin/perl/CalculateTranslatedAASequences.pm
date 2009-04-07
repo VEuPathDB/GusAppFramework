3ae18e3201681b4f09e7787ab005fabe5a76e4c8
Vishal Nayak <vishalkn@_.com>
2009-04-07 02:54:34 +0000

    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/GusAppFramework/trunk@6974 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Supported/plugin/perl/CalculateTranslatedAASequences.pm
+++ Supported/plugin/perl/CalculateTranslatedAASequences.pm
@@ -104,7 +104,7 @@ sub new {
   bless $self, $class;
 
   $self->initialize({ requiredDbVersion => 3.5,
-		      cvsRevision =>  '$Revision$',
+		      cvsRevision =>  '',
 		      name => ref($self),
 		      argsDeclaration   => $argsDeclaration,
 		      documentation     => $documentation
