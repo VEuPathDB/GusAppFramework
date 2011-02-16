574e0a4e0f89a5ac503ad4fad145995a85ac4a6b
Elisabetta Manduchi <manduchi@upenn.edu>
2011-02-16 20:27:15 +0000

    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/GusAppFramework/trunk@9084 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Supported/plugin/perl/LoadArrayDesign.pm
+++ Supported/plugin/perl/LoadArrayDesign.pm
@@ -95,7 +95,7 @@ ga GUS::Supported::Plugin::LoadArrayDesign B<[options]> B<--cfg_file> cfg_file B
 
 This file tells the plug-in how to map table/view attributes to columns in the data file and gives the values for attributes in ArrayDesign, ArrayDesignAnnotation.
 
-See the sample config file F<sample_AnalysisResultLoader.cfg> in the GUS/RAD/config directory.
+See the sample config file F<sample_LoadArrayDesign.cfg> in the GUS/RAD/config directory.
 
 Empty lines are ignored.
 
@@ -106,8 +106,6 @@ Do not use special symbols (like NA or similar) for empty fields: either leave t
 The names of each field and instructions for their values are as follows: 
 
 This configuration file tells the plug-in how to map table/view attributes to columns in the data file. Here is a detailed description of the format:
-
-You should save this file in the CVS repository, in the directory PROJECT_HOME/RAD/DataLoad/config (the name of this file, stripped of its path, is stored by the ga as an algorithm parameter).
 
 * This should be a tab-delimited text file, with 2 columns, named: "Table.attribute", "value or header" The order and case of these columns is important, and it is recommended to follow the template for the sake of consistency.
 
