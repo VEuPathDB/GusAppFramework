9924334f89209e76d239f84cb7708938ce0bc16f
Wei Li <weili1@pennmedicine.upenn.edu>
2011-04-18 20:30:00 +0000

    change Db Version to 3.6
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/GusAppFramework/trunk@9229 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Supported/plugin/perl/dbEST.pm
+++ Supported/plugin/perl/dbEST.pm
@@ -182,7 +182,7 @@ FAIL_CASES
 	       })
     ];
 
-  $self->initialize({requiredDbVersion => 3.5,
+  $self->initialize({requiredDbVersion => 3.6,
 		     cvsRevision => '$Revision$',	# cvs fills this in!
 		     name => ref($self),
 		     argsDeclaration   => $argsDeclaration,
