e21491975915a91e3aa2cc518583dbedb4269878
Wei Li <weili1@pennmedicine.upenn.edu>
2011-05-24 15:26:35 +0000

    change DbVersion to 3.6
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/GusAppFramework/trunk@9312 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- PluginMgr/lib/perl/GusApplication.pm
+++ PluginMgr/lib/perl/GusApplication.pm
@@ -63,7 +63,7 @@ sub new {
 
    my $self = bless {}, $Class;
 
-   $self->initialize({requiredDbVersion => '3.5',
+   $self->initialize({requiredDbVersion => '3.6',
                       cvsRevision       => '$Revision$',
                       name              => ref($self),
                       easyCspOptions    => {},
