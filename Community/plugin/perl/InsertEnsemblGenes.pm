2e367bff96ff6cd5e26f29ce52797fb794d0b869
Elisabetta Manduchi <manduchi@upenn.edu>
2013-03-04 19:03:50 +0000

    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/GusAppFramework/branches/4.0@11554 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Community/plugin/perl/InsertEnsemblGenes.pm
+++ Community/plugin/perl/InsertEnsemblGenes.pm
@@ -110,7 +110,7 @@ sub new {
   my $argumentDeclaration    = &getArgumentsDeclaration();
 
   $self->initialize({requiredDbVersion => 4.0,
-		     cvsRevision => '$Revision: 11552 $',
+		     cvsRevision => '$Revision: 11553 $',
 		     name => ref($self),
 		     revisionNotes => '',
 		     argsDeclaration => $argumentDeclaration,
@@ -243,9 +243,9 @@ sub insertGenes {
     $geneInstance->setParent($geneFeature);
     my $geneNaLocation = GUS::Model::DoTS::NALocation->new({start_min => $geneStart, start_max => $geneStart, end_min => $geneEnd, end_max => $geneEnd, is_reversed => $isReversed}); 
     $geneNaLocation->setParent($geneFeature);
-    $gene->addToSubmitList($geneFeature);
     my $rnaFeature = GUS::Model::DoTS::RNAFeature->new({name => $rnaId, na_sequence_id => $chrId, external_database_release_id => $extDbRlsGenes, source_id => $rnaId});
     $rnaFeature->setParent($geneFeature);
+    $geneFeature->addToSubmitList($rnaFeature);
     my $rnaNaLocation = GUS::Model::DoTS::NALocation->new({start_min => $rnaStart, start_max => $rnaStart, end_min => $rnaEnd, end_max => $rnaEnd, is_reversed => $isReversed}); 
     $rnaNaLocation->setParent($rnaFeature);
     my $exonId;
