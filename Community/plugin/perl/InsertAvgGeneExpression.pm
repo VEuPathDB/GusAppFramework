3220c8a29813f84334aab10815d30a57478503ea
Elisabetta Manduchi <manduchi@upenn.edu>
2013-08-02 14:25:19 +0000

    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/GusAppFramework/branches/4.0@12618 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Community/plugin/perl/InsertAvgGeneExpression.pm
+++ Community/plugin/perl/InsertAvgGeneExpression.pm
@@ -76,7 +76,7 @@ sub new {
   my $argumentDeclaration    = &getArgumentsDeclaration();
 
   $self->initialize({requiredDbVersion => 4.0,
-		     cvsRevision => '$Revision: 12616  $',
+		     cvsRevision => '$Revision: 12617  $',
 		     name => ref($self),
 		     revisionNotes => '',
 		     argsDeclaration => $argumentDeclaration,
@@ -108,7 +108,7 @@ sub insertResults {
   my $protAppNodeId = $self->getArg('protAppNodeId');
 
   my $dbh = $self->getQueryHandle();
-  my $sth = $dbh->prepareAndExecute("select gi.gene_id, avg(e.value), stddev(e.value), count(e.value) from dots.geneinstance gi, results.nafeatureexpression e where gi.na_feature_id=e.na_feature_id and e.protocol_app_node_id=$protAppNodeId group by gi.gene_id");
+  my $sth = $dbh->prepareAndExecute("select gi.gene_id, avg(e.value), stddev(e.value), count(e.value) from dots.geneinstance gi, results.nafeatureexpression e where gi.na_feature_id=e.na_feature_id and e.protocol_app_node_id=$protAppNodeId and gi.na_feature_id not in (select na_feature_id from dots.genefeature where name like 'UNIQUE%') group by gi.gene_id");
   while (my ($geneId, $avg, $stdDev, $count)=$sth->fetchrow_array()) {
     my $geneExpr = GUS::Model::Results::GeneExpression->new({gene_id => $geneId, protocol_app_node_id => $protAppNodeId, value => $avg, standard_error => $stdDev/$count});
     $geneExpr->submit();
