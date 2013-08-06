7642a8ed38958ae922083f0cdf5f837378fe0884
Elisabetta Manduchi <manduchi@upenn.edu>
2013-08-06 20:53:42 +0000

    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/GusAppFramework/branches/4.0@12633 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Community/plugin/perl/InsertEnsemblGenes.pm
+++ Community/plugin/perl/InsertEnsemblGenes.pm
@@ -117,7 +117,7 @@ sub new {
   my $argumentDeclaration    = &getArgumentsDeclaration();
 
   $self->initialize({requiredDbVersion => 4.0,
-		     cvsRevision => '$Revision: 12629 $',
+		     cvsRevision => '$Revision: 12633 $',
 		     name => ref($self),
 		     revisionNotes => '',
 		     argsDeclaration => $argumentDeclaration,
