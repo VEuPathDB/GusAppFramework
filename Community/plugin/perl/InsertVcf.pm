1693216c4f7e43933dbbf4649881a93c8585e537
John Iodice <iodice@pennmedicine.upenn.edu>
2013-10-24 13:19:29 +0000

    propset
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/GusAppFramework/branches/4.0@12982 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Community/plugin/perl/InsertVcf.pm
+++ Community/plugin/perl/InsertVcf.pm
@@ -93,7 +93,7 @@ sub new {
   my $argumentDeclaration    = &getArgumentsDeclaration();
 
   $self->initialize({requiredDbVersion => 4.0,
-		     cvsRevision => '$Revision: $',
+		     cvsRevision => '$Revision$',
 		     name => ref($self),
 		     revisionNotes => '',
 		     argsDeclaration => $argumentDeclaration,
