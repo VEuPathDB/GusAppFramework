5cc53bf0472c554c8c0328dd12d204c5837b3726
John Iodice <iodice@pennmedicine.upenn.edu>
2013-10-24 12:45:48 +0000

    tinkering with the revision number to try to make GUS happy
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/GusAppFramework/branches/4.0@12979 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Community/plugin/perl/InsertVcf.pm
+++ Community/plugin/perl/InsertVcf.pm
@@ -93,7 +93,7 @@ sub new {
   my $argumentDeclaration    = &getArgumentsDeclaration();
 
   $self->initialize({requiredDbVersion => 4.0,
-		     cvsRevision => '$Revision: 12831 $',
+		     cvsRevision => '$Revision: $',
 		     name => ref($self),
 		     revisionNotes => '',
 		     argsDeclaration => $argumentDeclaration,
