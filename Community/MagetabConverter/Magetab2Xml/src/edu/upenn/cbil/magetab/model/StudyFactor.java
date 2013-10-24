088c15423dba84ad5ba8a59c2b79276bc5a643dd
Cris Lawrence <crisl@upenn.edu>
2013-10-24 20:42:37 +0000

    Altered rules....if a study factor is an addition, so are the corresponding factor values, regardless of highlight.  They won't be show as additions if the parent node is itself an addition.
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/GusAppFramework/branches/4.0@12998 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Community/MagetabConverter/Magetab2Xml/src/edu/upenn/cbil/magetab/model/StudyFactor.java
+++ Community/MagetabConverter/Magetab2Xml/src/edu/upenn/cbil/magetab/model/StudyFactor.java
@@ -1,17 +1,24 @@
 package edu.upenn.cbil.magetab.model;
 
 import java.lang.reflect.Field;
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import org.apache.commons.lang.builder.ReflectionToStringBuilder;
 import org.apache.commons.lang.builder.ToStringStyle;
+import org.jdom2.Document;
+import org.jdom2.Element;
+
+import edu.upenn.cbil.limpopo.utils.AppUtils;
 
 public class StudyFactor {
   private String name;
   private boolean addition;
   private static Set<String> names = new HashSet<>();
   private static Set<String> addedNames = new HashSet<>();
+  private static List<StudyFactor> studyFactors = new ArrayList<>();
   
   public StudyFactor(String name, boolean addition) {
     this.name = name;
@@ -36,6 +43,28 @@ public class StudyFactor {
   
   public static Set<String> getAddedNames() {
     return addedNames;
+  }
+  
+  public static void populate(Document document) {
+    List<Element> studyFactorElements = document.getRootElement().getChildren(AppUtils.IDF_TAG).get(0).getChildren(AppUtils.STUDY_FACTOR_TAG);
+    for(Element studyFactorElement : studyFactorElements) {
+      boolean addition = false;
+      if(studyFactorElement.getAttributeValue(AppUtils.ADDITION_ATTR) != null) {
+        addition = true;
+      }
+      StudyFactor studyFactor = new StudyFactor(studyFactorElement.getChildText(AppUtils.NAME_TAG),addition);
+      studyFactors.add(studyFactor);
+    }
+  }
+  
+  public static StudyFactor getStudyFactorByName(String name) {
+    for(StudyFactor studyFactor : studyFactors) {
+      System.out.println(studyFactor.getName());
+      if(name.equals(studyFactor.getName())) {
+        return studyFactor;
+      }
+    }
+    return null;
   }
   
   public String toString() {
