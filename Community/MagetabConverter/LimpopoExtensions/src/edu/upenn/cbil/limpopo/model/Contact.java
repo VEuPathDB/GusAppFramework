ffff5238593114f8d147a3ae04db4ae51854a5d3
Cris Lawrence <crisl@upenn.edu>
2013-10-29 20:09:25 +0000

    Minor cleanup and documentation.
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/GusAppFramework/branches/4.0@13010 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Community/MagetabConverter/LimpopoExtensions/src/edu/upenn/cbil/limpopo/model/Contact.java
+++ Community/MagetabConverter/LimpopoExtensions/src/edu/upenn/cbil/limpopo/model/Contact.java
@@ -53,106 +53,257 @@ public class Contact {
   public static final String COUNTRY_COMMENT = "Country";
   public static final String ZIP_COMMENT = "Zip";
 
-  public Contact() {
-  }
-  
+  /**
+   * Indicates whether or not this contact represents an addition (as determined by spreadsheet highlighting)
+   * @return - true if an addition and false otherwise
+   */
   public final boolean isAddition() {
     return addition;
   }
+  
+  /**
+   * Sets the addition flag
+   * @param addition - true if addition, false otherwise
+   */
   public final void setAddition(boolean addition) {
     this.addition = addition;
   }
+  
+  /**
+   * Provides first name of contact
+   * @return - string representing first name of contact.  May be null or empty.
+   */
   public final String getFirstName() {
     return firstName;
   }
-  public final void setFirstName(final String firstName) {
+  
+  /**
+   * Sets the first name of the contact, removing any addition token.
+   * @param firstName - raw first name of contact data (with possible addition token)
+   */
+  protected final void setFirstName(final String firstName) {
     this.firstName = AppUtils.removeTokens(firstName);
   }
+  
+  /**
+   * Provide last name of contact
+   * @return - string representing last name of contact.  May be null of empty.
+   */
   public final String getLastName() {
     return lastName;
   }
-  public final void setLastName(final String lastName) {
+  
+  /**
+   * Sets the last name of the contact, removes any addition token.
+   * @param lastName - raw last name of contact data (with possible addition token)
+   */
+  protected final void setLastName(final String lastName) {
     this.lastName = AppUtils.removeTokens(lastName);
   }
+  
+  /**
+   * Provides the laboratory name.
+   * @return - string representing the laboratory name.  May be null of empty.
+   */
   public final String getLab() {
     return lab;
   }
-  public final void setLab(final String lab) {
+  
+  /**
+   * Comment [Lab] header.  Sets the laboratory name of the contact, removing any addition token.
+   * @param lab - raw laboratory name data (with possible addition token)
+   */
+  protected final void setLab(final String lab) {
     this.lab = AppUtils.removeTokens(lab);
   }
+  
   public final OntologyTerm getRoles() {
     return roles;
   }
   public final void setRoles(final OntologyTerm roles) {
     this.roles = roles;
   }
+  
+  /**
+   * Provides the contact's email address
+   * @return - string representing the contact email.  May be null or empty.
+   */
   public final String getEmail() {
     return email;
   }
-  public final void setEmail(final String email) {
+  
+  /**
+   * Sets the email address of the contact, removing any addition token.
+   * @param email - raw email address data (with possible addition token)
+   */
+  protected final void setEmail(final String email) {
     this.email = AppUtils.removeTokens(email);
   }
+  
+  /**
+   * Provides the contact's phone number
+   * @return - string representing the phone number.  May be null or empty.
+   */
   public final String getPhone() {
     return phone;
   }
-  public final void setPhone(final String phone) {
+  
+  /**
+   * Sets the phone number of the contact, removing any addition token.
+   * @param phone - raw phone number data (with possible addition token)
+   */
+  protected final void setPhone(final String phone) {
     this.phone = AppUtils.removeTokens(phone);
   }
+  
+  /**
+   * Provides the contact's fax number
+   * @return - string representing the fax number.  May be null or empty.
+   */
   public final String getFax() {
     return fax;
   }
-  public final void setFax(final String fax) {
+  
+  /**
+   * Sets the fax number of the contact, removing any addition token.
+   * @param fax - raw fax number data (with possible addition token)
+   */
+  protected final void setFax(final String fax) {
     this.fax = AppUtils.removeTokens(fax);
   }
+  
+  /**
+   * Provides the first line of the contact's street address
+   * @return - string representing the street address.  May be null or empty.
+   */
   public final String getAddress1() {
     return address1;
   }
+  
+  /**
+   * Sets the first line of the street address of the contact, removing any addition token.
+   * @param address1 - raw first line of street address data (with possible addition token)
+   */
   public final void setAddress1(final String address1) {
     this.address1 = AppUtils.removeTokens(address1);
   }
+  
+  /**
+   * Comment [Address2] header.  Provides the second line of the contact's street address.
+   * @return - string representing the 2nd line of the street address.  May be null or empty.
+   */
   public final String getAddress2() {
     return address2;
   }
-  public final void setAddress2(final String address2) {
+  
+  /**
+   * Sets the 2nd line of the street address of the contact, removing any addition token.
+   * @param address2 - raw second line of street address data (with possible addition token)
+   */
+  protected final void setAddress2(final String address2) {
     this.address2 = AppUtils.removeTokens(address2);
   }
+  
+  /**
+   * Comment [City] header.  Provides the contact's city.  
+   * @return - string representing the city.  May be null or empty.
+   */
   public final String getCity() {
     return city;
   }
-  public final void setCity(final String city) {
+  
+  /**
+   * Sets the contact's city, removing any addition token.
+   * @param city - raw city data (with possible addition token)
+   */
+  protected final void setCity(final String city) {
     this.city = AppUtils.removeTokens(city);
   }
+  
+  /**
+   * Comment [State] header.  Provides the contact's state.
+   * @return - string representing the state.  May be null or empty.
+   */
   public final String getState() {
     return state;
   }
-  public final void setState(final String state) {
+  
+  /**
+   * Sets the contact's state, removing any addition token.
+   * @param state - raw state data (with possible addition token)
+   */
+  protected final void setState(final String state) {
     this.state = AppUtils.removeTokens(state);
   }
+  
+  /**
+   * Comment [Country] header.  Provides the contact's country.
+   * @return - string representing the country.  May be null or empty.
+   */
   public final String getCountry() {
     return country;
   }
-  public final void setCountry(final String country) {
+  
+  /**
+   * Sets the contact's country, removing any addition token.
+   * @param country - raw country data (with possible addition token)
+   */
+  protected final void setCountry(final String country) {
     this.country = AppUtils.removeTokens(country);
   }
+  
+  /**
+   * Provides the contact's affiliation.
+   * @return - string representing the affiliation.  May be null or empty.
+   */
   public final String getAffiliation() {
     return affiliation;
   }
-  public final void setAffiliation(final String affiliation) {
+  
+  /**
+   * Sets the contact's affiliation, removing any addition token.
+   * @param affiliation - raw affiliation data (with possible addition token)
+   */
+  protected final void setAffiliation(final String affiliation) {
     this.affiliation = AppUtils.removeTokens(affiliation);
   }
+  
+  /**
+   * Provides the contact's full name.
+   * @return - string containing either first + last name, or last name or lab
+   */
   public String getName() {
     return name;
   }
-  private void setName() {
+  
+  /**
+   * Sets the contact's full name using firstName, lastName and lab data.  Addition
+   * tokens will be removed if present.
+   */
+  protected void setName() {
     name = createName(firstName, lastName, lab);
   }
+  
+  /**
+   * Comment [Zip] header.  Provides the contact's postal code.
+   * @return - string representing the postal code.  May be null or empty.
+   */
   public String getZipcode() {
     return zipcode;
   }
+  
+  /**
+   * Sets the contact's postal code, removing any addition token.
+   * @param zipcode - raw postal code (with possible addition token)
+   */
   public void setZipcode(String zipcode) {
     this.zipcode = AppUtils.removeTokens(zipcode);
   }
 
+  /**
+   * Helper method for diagnostic use.  Provides a more complete accounting of
+   * a contact's field values.
+   */
   @Override
   public String toString() {
     return (new ReflectionToStringBuilder(this, ToStringStyle.MULTI_LINE_STYLE) {
@@ -163,16 +314,17 @@ public class Contact {
   }
   
   /**
-   * Assembles a full contact name from IDF components.  Addition/id tokens are filtered out.
+   * Assembles a full contact name from first name, last name, lab data.  Any addition tokens are
+   * removed.
    * @param first - first name (may be empty)
    * @param last - last name (either this or laboratory name must be non-empty)
    * @param lab - laboratory name (either this or last name must be non-empty)
    * @return - the full contact name to be used
    */
   public static final String createName(final String first, final String  last, final String lab) {
-    String name = last;
-    if(StringUtils.isEmpty(last)) name = lab;
-    else if(StringUtils.isNotEmpty(first)) name = first + " " + last;
+    String name = AppUtils.removeTokens(last);
+    if(StringUtils.isEmpty(name)) name = lab;
+    else if(StringUtils.isNotEmpty(AppUtils.removeTokens(first))) name = first + " " + last;
     return AppUtils.removeTokens(name);
   }
 
@@ -221,6 +373,14 @@ public class Contact {
     return contactNames;
   }
 
+  /**
+   * Generates a list of fully populated contact objects from the IDF.  Only the contact
+   * last name is checked for an addition token.  If found, that contact is notes as an
+   * addition.
+   * @param data - IDF data
+   * @return - List of contacts acquired from IDF
+   * @throws ConversionException - caused by problems with ontology terms.
+   */
   public static List<Contact> populate(final IDF data) throws ConversionException {
 	logger.debug("START: Populating Contacts");
     List<String> labs = OrderedComment.retrieveComments(LAB_COMMENT, data);
