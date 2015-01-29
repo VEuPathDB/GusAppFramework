f8dbe5af057158cb802eb99d6d593dc6e8492304
Sufen Hu <sufenhu@pennmedicine.upenn.edu>
2015-01-29 22:33:19 +0000

    fix Revision
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/GusAppFramework/branches/4.0@15405 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Supported/plugin/perl/InsertTaxonAndTaxonName.pm
+++ Supported/plugin/perl/InsertTaxonAndTaxonName.pm
@@ -133,7 +133,7 @@ sub new {
   my $argsDeclaration = &getArgumentsDeclaration();
 
   $self->initialize({requiredDbVersion => 3.6,
-		     cvsRevision => '$Revision: 14716 $', # cvs fills this in!
+		     cvsRevision => '$Revision: 15381 $', # cvs fills this in!
 		     name => ref($self),
 		     argsDeclaration   => $argsDeclaration,
 		     documentation     => $documentation
