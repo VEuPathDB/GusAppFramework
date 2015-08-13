187738a10dcbff2d00c7b0fed47be4e2f325b8ca
Emily Greenfest-Allen <egreenfest@gmail.com>
2015-08-13 14:40:17 +0000

    added additional facilities to log nested merges to reduce overhead of restart
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/GusAppFramework/branches/4.0@16475 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Community/plugin/perl/InsertMergedSnpFeatureRelations.pm
+++ Community/plugin/perl/InsertMergedSnpFeatureRelations.pm
@@ -49,17 +49,17 @@ my $argsDeclaration =
 	       reqd           => 1,
 	       isList         => 0 }),
 
-  integerArg({ name  => 'skip',
-	       descr => "skip until line",
-	       constraintFunc => undef,
-	       reqd           => 0,
-	       isList         => 0 }),
+   integerArg({ name  => 'skip',
+		descr => "skip until line",
+		constraintFunc => undef,
+		reqd           => 0,
+		isList         => 0 }),
 
- booleanArg({ name  => 'skipAll',
-	       descr => "iterate over file to find nested merges, only load those",
-	       constraintFunc => undef,
-	       reqd           => 0,
-	       isList         => 0 }),
+   booleanArg({ name  => 'outputNestedMerges',
+		descr => "output nested merges as they are found; to facilitate RESTART",
+		constraintFunc => undef,
+		reqd           => 0,
+		isList         => 0 }),
  
   
   ];
@@ -84,11 +84,13 @@ my $tablesDependedOn = [
                      
                         ['SRes.ExternalDatabaseRelease', 'Lookups of external database specifications']
  
-];
+		       ];
 
 my $howToRestart = <<PLUGIN_RESTART;
-Restart with skip flag.  Plugin will still iterate over file to assemble hash to catch nested merges.  
-To skip all and just load nested merges, use the --skipAll option
+Restart with skip flag.  Plugin will not evaluate nested merges prior to the skip.
+
+Nested merges (ones requiring a second pass to create NAFeatureRelations) can be output as they are 
+found using the --outputNestedMerges flag.  File can then be used as the inFile to load only nested merges
 
 PLUGIN_RESTART
 
@@ -114,7 +116,7 @@ sub new {
 
 
   $self->initialize({requiredDbVersion => 4.0,
-		     cvsRevision => '$Revision: 16462 $', # cvs fills this in!
+		     cvsRevision => '$Revision: 16463 $', # cvs fills this in!
 		     name => ref($self),
 		     argsDeclaration => $argsDeclaration,
 		     documentation => $documentation
@@ -139,82 +141,86 @@ sub run {
   my $submitCount = 0;
   my $missingTargets = undef;
   my $missingTargetCount = 0;
-
   my $rowCount = 0;
-  my $skip = $self->getArg('skip');
-  my $skipAll = $self->getArg('skipAll');
+  my $nestedMergeFile = undef;
 
+  my $skip = $self->getArg('skip');
   my $fName = $self->getArg('inFile');
 
+  if ($self->getArg('outputNestedMerges')) {
+    $nestedMergeFile = FileHandle->new;
+    $nestedMergeFile->open(">$fName-nested-merges.txt") || $self->error("Unable to create nested merge file: $fName-nested-merges.txt");
+  }
+
   my $fh = FileHandle->new;
-  $fh->open("<$fName") || $self->error("Unable to open input file: $fName\n");
+  $fh->open("<$fName") || $self->error("Unable to open input file: $fName.");
   while (<$fh>) {
-      my @values = split /\t/;
-      chomp(@values);
-
-      $rowCount += 1;
-
-      unless ($rowCount % 5000) {
-	$self->undefPointerCache(); # need to clear the pointer cache
-	$self->log("SKIPPED $rowCount rows.") if ($skipAll or $rowCount <= $skip) ;
-      }
-
-      # dbSnp merges high to low; rsHigh is the id that was merged
-      my $rsHigh = 'rs' . $values[0]; # rsHigh
-      my $rsLow = 'rs' . $values[1]; # rsLow
-
-      my $lowSnpFeature = $self->retrieveSnpFeature($rsLow, $xdbrId);
-      unless ($lowSnpFeature->retrieveFromDB()) {
-	# the lowSnpFeature might not be in the DB -- either it is itself later merged in the file
-	# or it is defined against a different genome build (dbSNP releases may be for multiple builds)
-	# so save for later; cannot distinguish without second pass
-	$missingTargetCount++;
-	$missingTargets->{$rsHigh} = $rsLow;
-	$self->log("Merge target SNP $rsLow for $rsHigh not found in DB. Saving for second pass.")
-	  if ($self->getArg('veryVerbose'));
-	next;
-      }
-
-      next if ($skipAll);
-      next if ($rowCount <= $skip);
-      $self->log("SKIPPED $rowCount rows.  Resuming load with $rsHigh -> $rsLow.") if ($rowCount > $skip);
-
-      # create a new entry in SnpFeature for the high snp
-      my $highSnpFeature = $self->retrieveSnpFeature($rsHigh, $xdbrId);
-    
-      unless ($highSnpFeature->retrieveFromDB()) {
-	$highSnpFeature->setName('merged');
-	$highSnpFeature->submit(undef, 1); # noTran = 1 --> do not commit at this point
-	$submitCount++;
-      }
-
-      # create the feature relationships
-      my $featureRelation = GUS::Model::DoTS::NAFeatureRelationship->new({
-            parent_na_feature_id => $lowSnpFeature->getNaFeatureId(),
-            child_na_feature_id => $highSnpFeature->getNaFeatureId(),
-            na_feat_relationship_type_id => $relationshipTypeId
-									 });
-
-      $featureRelation->submit(undef, 1); # noTran = 1 --> do not commit at this point
-      $submitCount += 1;
-
-      unless ($submitCount % 1000) {
-	if ($self->getArg("commit")) {
-	  $self->getDb()->manageTransaction(undef, "commit"); # commit
-	  $self->getDb()->manageTransaction(undef, "begin");
-	}
-	
-	$self->log("$submitCount records loaded.")
-	  if $self->getArg('verbose');
-      }
+    unless (++$rowCount % 5000) {
+      $self->log("SKIPPED $rowCount rows.") if ($rowCount <= $skip) ;
     }
+    next if ($rowCount <= $skip);
+
+    my @values = split /\t/;
+    chomp(@values);
+
+    # dbSnp merges high to low; rsHigh is the id that was merged
+    my $rsHigh = 'rs' . $values[0];  # rsHigh
+    my $rsLow = 'rs' . $values[1];   # rsLow
+
+    my $lowSnpFeature = $self->retrieveSnpFeature($rsLow, $xdbrId);
+    unless ($lowSnpFeature->retrieveFromDB()) {
+      # the lowSnpFeature might not be in the DB -- either it is itself later merged in the file
+      # or it is defined against a different genome build (dbSNP releases may be for multiple builds)
+      # so save for later; cannot distinguish without second pass
+      $missingTargetCount++;
+      $missingTargets->{$rsHigh} = $rsLow;
+      $self->log("Merge target SNP $rsLow for $rsHigh not found in DB. Saving for second pass.")
+	if ($self->getArg('veryVerbose'));
+
+      print $nestedMergeFile join("\t", $rsHigh, $rsLow) . "\n" if ($nestedMergeFile);	
+      next;
+    }
+
+    # create a new entry in SnpFeature for the high snp
+    my $highSnpFeature = $self->retrieveSnpFeature($rsHigh, $xdbrId);
+    
+    unless ($highSnpFeature->retrieveFromDB()) {
+      $highSnpFeature->setName('merged');
+      $highSnpFeature->submit(undef, 1); # noTran = 1 --> do not commit at this point
+      $submitCount++;
+    }
+
+    # create the feature relationships
+    my $featureRelation = GUS::Model::DoTS::NAFeatureRelationship->new({
+									parent_na_feature_id => $lowSnpFeature->getNaFeatureId(),
+									child_na_feature_id => $highSnpFeature->getNaFeatureId(),
+									na_feat_relationship_type_id => $relationshipTypeId
+								       });
+
+    $featureRelation->submit(undef, 1); # noTran = 1 --> do not commit at this point
+    $submitCount += 1;
+
+    unless ($submitCount % 1000) {
+      if ($self->getArg("commit")) {
+	$self->getDb()->manageTransaction(undef, "commit"); # commit
+	$self->getDb()->manageTransaction(undef, "begin");
+      }
+
+      $self->undefPointerCache();
+
+      $self->log("$submitCount records loaded.")
+	if $self->getArg('verbose');
+    }
+  }
 
   if ($self->getArg("commit")) {
     $self->getDb()->manageTransaction(undef, "commit"); # commit final batch
     $self->log("$submitCount records loaded.") if $self->getArg('verbose');
+    $self->undefPointerCache();
   }
   
   $fh->close();
+  $nestedMergeFile->close() if ($nestedMergeFile);
 
   # make pass over targets (lowSnps) that were not found in DB, but may have been added 
   # as a merged snp
@@ -226,42 +232,40 @@ sub insertMissedTargets {
   my ($self, $mergeRelations, $xdbrId, $relationshipTypeId) = @_;
   my $submitCount = 0;
 
-  while  (my ($rsHigh, $rsLow) = each %$mergeRelations) {
+  while (my ($rsHigh, $rsLow) = each %$mergeRelations) {
+
     my $lowSnpFeature = $self->retrieveSnpFeature($rsLow, $xdbrId);
     next unless ($lowSnpFeature->retrieveFromDB()); # if lowSNP is not in the DB, it must be from another genome build, skip
-      
+
     # create a new entry in SnpFeature for the high snp
     my $highSnpFeature = $self->retrieveSnpFeature($rsHigh, $xdbrId);
-      
     unless ($highSnpFeature->retrieveFromDB()) { # if we resumed, this may have already been loaded
-	$highSnpFeature->setName('merged');
-	$highSnpFeature->submit(undef, 1); # noTran = 1 --> do not commit at this point
-	$submitCount++;
+      $highSnpFeature->setName('merged');
+      $highSnpFeature->submit(undef, 1); # noTran = 1 --> do not commit at this point
+      $submitCount++;
     }
- 
-     
+
     # create the feature relationships
     my $featureRelation = GUS::Model::DoTS::NAFeatureRelationship->new({
-	parent_na_feature_id => $lowSnpFeature->getNaFeatureId(),
-	child_na_feature_id => $highSnpFeature->getNaFeatureId(),
-	na_feat_relationship_type_id => $relationshipTypeId
+									parent_na_feature_id => $lowSnpFeature->getNaFeatureId(),
+									child_na_feature_id => $highSnpFeature->getNaFeatureId(),
+									na_feat_relationship_type_id => $relationshipTypeId
 								       });
-    unless ($featureRelation->retrieveFromDB()) {# again, if resumed, this may have already been loaded
-	$featureRelation->submit(undef, 1); # noTran = 1 --> do not commit at this point
-	$submitCount += 1;
+    unless ($featureRelation->retrieveFromDB()) { # again, if resumed, this may have already been loaded
+      $featureRelation->submit(undef, 1); # noTran = 1 --> do not commit at this point
+      $submitCount += 1;
     }
-    
+
     unless ($submitCount % 1000) {
-	if ($self->getArg("commit")) {
-	    $self->getDb()->manageTransaction(undef, "commit"); # commit
-	    $self->getDb()->manageTransaction(undef, "begin");
-	}
-	
-	$self->undefPointerCache();
-	$self->log("$submitCount records loaded.")
-	    if $self->getArg('verbose');
+      if ($self->getArg("commit")) {
+	$self->getDb()->manageTransaction(undef, "commit"); # commit
+	$self->getDb()->manageTransaction(undef, "begin");
+      }
+      $self->undefPointerCache();
+      $self->log("$submitCount records loaded.")
+	if $self->getArg('verbose');
     }
-  } # end iterate over hash
+  }				# end iterate over hash
 
   if ($self->getArg("commit")) {
     $self->getDb()->manageTransaction(undef, "commit"); # commit final batch
@@ -282,23 +286,23 @@ sub retrieveSnpFeature {
 }
 
 sub fetchRelationshipTypeId {
-    my ($self) = @_;
+  my ($self) = @_;
 
-    my $type = $self->getArg('type');
-    my $relationshipType = GUS::Model::DoTS::NAFeatRelationshipType->new({name => $type});
-    unless ($relationshipType->retrieveFromDB()) {
-      $relationshipType->setDescription($type); # description can't be null so just use same value
-      $relationshipType->submit();
-    }
+  my $type = $self->getArg('type');
+  my $relationshipType = GUS::Model::DoTS::NAFeatRelationshipType->new({name => $type});
+  unless ($relationshipType->retrieveFromDB()) {
+    $relationshipType->setDescription($type); # description can't be null so just use same value
+    $relationshipType->submit();
+  }
 
-    return $relationshipType->getNaFeatRelationshipTypeId();
+  return $relationshipType->getNaFeatRelationshipTypeId();
 }
 
 sub undoTables {
   my ($self) = @_;
 
   return ('DoTS.NAFeatureRelationship',
-          'DoTS.SnpFeature');  # snp feature won't work
+          'DoTS.SnpFeature');	# snp feature won't work
 
 }
 
