ce710373f56dca59ccac0eae764bc4baffb03ccb
Deborah Pinney <pinney@upenn.edu>
2015-09-25 13:20:42 +0000

    remove description update for ExtNASeq - contains sequence so not needed and causes failure with some db configs
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/GusAppFramework/branches/4.0@16787 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Supported/plugin/perl/dbEST.pm
+++ Supported/plugin/perl/dbEST.pm
@@ -1286,7 +1286,7 @@ sub checkExtNASeq {
                    't_count' => (scalar($e->{sequence} =~ s/T/T/g)) || 0,
                    'c_count' => (scalar($e->{sequence} =~ s/C/C/g)) || 0,
                    'g_count' => (scalar($e->{sequence} =~ s/G/G/g)) || 0,
-                   'description' => substr($e->{comment},0,1999)
+      # contains sequence - unneeded and fails with some db configurations  'description' => substr($e->{comment},0,1999)
                    );
   foreach my $a (keys %seq_vals) {
     if ( $seq->isValidAttribute($a)) {
