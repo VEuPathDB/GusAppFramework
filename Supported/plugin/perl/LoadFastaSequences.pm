7b1ab3785d39a35db7b1dd72973f0849dcea3d94
Haiming Wang <hwang@uga.edu>
2017-03-09 17:36:42 +0000

    change logFrequency default to 100 to reduce the amount of log messages - refs #23570
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/GusAppFramework/trunk@19838 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Supported/plugin/perl/LoadFastaSequences.pm
+++ Supported/plugin/perl/LoadFastaSequences.pm
@@ -79,7 +79,7 @@ stringArg({   name           => 'ncbiTaxonName',
  integerArg({   name          => 'logFrequency',
 	       descr          => 'The frequency of logging progress, ie, after how many sequences are processed',
 	       reqd           => 0,
-		default       => 10,
+		default       => 100,
 	       constraintFunc => undef,
 	       isList         => 0 }),
 
